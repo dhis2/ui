c00f65f7c0c9ad333826989fe8ff3e9a115f6325
Viktor Varland <varl@vlv.io>
2020-03-06 17:32:54 +0100

    refactor(modalactions): colocate code and stories

--- packages/core/src/ModalActions/ModalActions.stories.e2e.js
+++ packages/core/src/ModalActions/ModalActions.stories.e2e.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { ModalActions } from '../index.js'
+import { ModalActions } from './ModalActions.js'
 
 storiesOf('ModalActions', module).add('With children', () => (
     <ModalActions>I am a child</ModalActions>
