9e4c460baceb4dd2ba310149378c337ec74dacee
Viktor Varland <varl@vlv.io>
2020-03-06 17:46:48 +0100

    test(modalcontent): repair broken import in e2e

--- packages/core/src/ModalContent/ModalContent.stories.e2e.js
+++ packages/core/src/ModalContent/ModalContent.stories.e2e.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { ModalContent } from './ModalContent/ModalContent.js'
+import { ModalContent } from './ModalContent.js'
 
 storiesOf('ModalContent', module).add('With children', () => (
     <ModalContent>I am a child</ModalContent>
