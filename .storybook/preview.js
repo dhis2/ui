7140d073c3bfec7d2cb6f9d0133207701053a011
Viktor Varland <varl@vlv.io>
2020-03-09 15:57:55 +0100

    Squashed 'packages/core/' changes from 803aa7f6..f06e9934
    f06e9934 docs: fix jsx not showing in storybook
    
    git-subtree-dir: packages/core
    git-subtree-split: f06e9934905480815c1ef8e2d6d1e12aef152dfa

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,34 @@
 import 'typeface-roboto'
+import React, { Fragment } from 'react'
+import { addDecorator } from '@storybook/react'
+import { jsxDecorator } from 'storybook-addon-jsx'
+import { CssReset } from '../src/index.js'
 
-import './decorators.js'
+addDecorator(jsxDecorator)
+
+addDecorator(fn => (
+    <Fragment>
+        <CssReset />
+        {fn()}
+    </Fragment>
+))
+
+addDecorator(fn => (
+    <Fragment>
+        {fn()}
+
+        <style jsx>{`
+            :global(html) {
+                height: 100%;
+            }
+            :global(body) {
+                height: 100%;
+                min-height: 100%;
+            }
+            :global(#root) {
+                height: 100%;
+                padding: 16px;
+            }
+        `}</style>
+    </Fragment>
+))
