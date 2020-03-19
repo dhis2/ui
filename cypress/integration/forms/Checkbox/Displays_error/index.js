a1850724482e6acb82ca3a26af686b2c3e8b16a3
Viktor Varland <varl@vlv.io>
2020-03-19 09:56:49 +0100

    ci(cypress): update project id

--- cypress/integration/forms/Checkbox/Displays_error/index.js
+++ cypress/integration/forms/Checkbox/Displays_error/index.js
@@ -1,4 +1,4 @@
-import '../common/index.js'
+import '../../common/submit.js'
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
 import { hasValueMessage } from '../../../../../packages/forms/src/validators/hasValue.js'
 
