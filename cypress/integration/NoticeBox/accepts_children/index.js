357ef6d45e739d53cf1cef7933ed121259016f54
ismay <ismay@ismaywolff.nl>
2020-04-07 11:30:36 +0200

    feat: add noticebox component

---
+++ cypress/integration/NoticeBox/accepts_children/index.js
@@ -0,0 +1,12 @@
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+
+Given('a NoticeBox receives a message as children', () => {
+    cy.visitStory('NoticeBox', 'With children')
+    cy.get('[data-test="dhis2-uicore-noticebox"]').should('be.visible')
+})
+
+Then('the message is visible', () => {
+    cy.get('[data-test="dhis2-uicore-noticebox-message"]')
+        .contains('The noticebox content')
+        .should('be.visible')
+})
