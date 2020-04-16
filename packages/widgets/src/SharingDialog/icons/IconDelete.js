8cd66b47fb53501c7d8966d4b4f0d9059b3f1537
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-04-16 16:33:59 +0200

    feat: Add DHIS2 Sharing Dialog

---
+++ packages/widgets/src/SharingDialog/icons/IconDelete.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const IconDelete = () => (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+      <g
+        id="Artboard"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <path
+          d="M6,19 C6,20.1 6.9,21 8,21 L16,21 C17.1,21 18,20.1 18,19 L18,7 L6,7 L6,19 Z M8.46,11.88 L9.87,10.47 L12,12.59 L14.12,10.47 L15.53,11.88 L13.41,14 L15.53,16.12 L14.12,17.53 L12,15.41 L9.88,17.53 L8.47,16.12 L10.59,14 L8.46,11.88 Z M15.5,4 L14.5,3 L9.5,3 L8.5,4 L5,4 L5,6 L19,6 L19,4 L15.5,4 Z"
+          id="Shape"
+          fill="#C62828"
+          fillRule="nonzero"
+        />
+      </g>
+    </svg>
+  );
