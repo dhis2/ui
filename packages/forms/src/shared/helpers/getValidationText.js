9f74e897f9ee60ae121b05e1442ff356aff9468a
ismay <ismay@ismaywolff.nl>
2020-04-22 17:11:13 +0200

    fix: display submit errors

--- packages/forms/src/shared/helpers/getValidationText.js
+++ packages/forms/src/shared/helpers/getValidationText.js
@@ -1,6 +1,21 @@
 import { hasError } from './hasError.js'
 
-const getValidationText = (meta, validationText, error) =>
-    validationText || (hasError(meta, error) && meta.error) || ''
+const getValidationText = (meta, validationText, error) => {
+    if (validationText) {
+        return validationText
+    }
+
+    if (hasError(meta, error)) {
+        if (meta.error) {
+            return meta.error
+        }
+
+        if (meta.submitError) {
+            return meta.submitError
+        }
+    }
+
+    return ''
+}
 
 export { getValidationText }
