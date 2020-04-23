7c0d070176bc1e4ac1320fd3a82784db25f0d953
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-04-23 16:18:12 +0200

    refactor: move layouts to separate package

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -10,6 +10,7 @@ module.exports = {
                 // add our src folders for webpack loaders
                 src.include.push(/packages\/core\/src/)
                 src.include.push(/packages\/forms\/src/)
+                src.include.push(/packages\/layouts\/src/)
                 src.include.push(/packages\/widgets\/src/)
             }
         }
@@ -21,11 +22,13 @@ module.exports = {
         ? [
             '@dhis2/ui-core/src/**/*.stories.e2e.js',
             '@dhis2/ui-forms/src/**/*.stories.e2e.js',
+            '@dhis2/ui-layouts/src/**/*.stories.e2e.js',
             '@dhis2/ui-widgets/src/**/*.stories.e2e.js',
         ]
         : [
             '@dhis2/ui-core/src/**/*.stories.js',
             '@dhis2/ui-forms/src/**/*.stories.js',
+            '@dhis2/ui-layouts/src/**/*.stories.js',
             '@dhis2/ui-widgets/src/**/*.stories.js',
         ],
 
