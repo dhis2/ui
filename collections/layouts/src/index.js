9bd6a6b78ef4641496669637acf3f0b207e5b957
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-04-23 16:18:12 +0200

    refactor: move layouts to separate package

---
+++ collections/layouts/src/index.js
@@ -0,0 +1,4 @@
+export { SidebarLayout } from './SidebarLayout/SidebarLayout'
+export { Sidebar } from './SidebarLayout/Sidebar'
+export { TopbarLayout } from './TopbarLayout/TopbarLayout'
+export { Topbar } from './TopbarLayout/Topbar'
