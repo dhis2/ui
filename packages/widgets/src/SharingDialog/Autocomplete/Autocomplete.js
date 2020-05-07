92dff16ac1fb2fe5658a089dd66e7e93de38d9cb
Edoardo Sabadelli <edoardo@dhis2.org>
2020-05-07 12:44:10 +0200

    feat: search as you type for user/groups
    WIP

---
+++ packages/widgets/src/SharingDialog/Autocomplete/Autocomplete.js
@@ -0,0 +1,110 @@
+import React, { createRef, useState, useEffect } from 'react'
+import propTypes from '@dhis2/prop-types'
+
+import { Menu, MenuItem } from '@dhis2/ui-core'
+import { InputField } from '../../'
+import { MenuWrapper } from './MenuWrapper'
+
+// Keycodes for the keypress event handlers
+// XXX implement keyboard navigation in the Menu ?!
+/*const ESCAPE_KEY = 27
+const SPACE_KEY = 32
+const UP_KEY = 38
+const DOWN_KEY = 40
+*/
+
+// XXX pass this whole component or the one that renders the MenuItem
+// from the app/parent to make it as flexible as possible
+const SearchResults = ({ searchResults, onClick }) => (
+    <Menu>
+        {searchResults.map(searchResult => (
+            <MenuItem
+                key={searchResult.id}
+                label={searchResult.displayName}
+                value={searchResult.id}
+                onClick={onClick}
+            />
+        ))}
+    </Menu>
+)
+
+SearchResults.propTypes = {
+    searchResults: propTypes.array,
+    onClick: propTypes.func,
+}
+
+export const Autocomplete = ({
+    placeholder,
+    onChange,
+    onClose,
+    onSearch,
+    dataTest,
+    maxHeight,
+    inputWidth,
+    value,
+    searchResults,
+}) => {
+    const inputRef = createRef()
+    const menuRef = createRef()
+
+    const [menuWidth, setMenuWidth] = useState('auto')
+
+    useEffect(() => {
+        if (inputRef.current) {
+            console.log('set menu width', inputRef.current.offsetWidth)
+            setMenuWidth(`${inputRef.current.offsetWidth}px`)
+        }
+    }, [])
+
+    // TODO debounce
+    const onInputChange = ({ value }) => {
+        onSearch(value)
+    }
+
+    const onSelect = ({ value }) => {
+        onChange(value)
+    }
+
+    return (
+        <div className="autocomplete-block" ref={menuRef}>
+            <div ref={inputRef}>
+                <InputField
+                    placeholder={placeholder}
+                    onChange={onInputChange}
+                    value={value}
+                    inputWidth={inputWidth}
+                />
+            </div>
+            {Boolean(searchResults.length) && (
+                <MenuWrapper
+                    onClick={onClose}
+                    maxHeight={maxHeight}
+                    menuRef={menuRef}
+                    menuWidth={menuWidth}
+                    dataTest={`${dataTest}-menu`}
+                >
+                    <SearchResults
+                        searchResults={searchResults}
+                        onClick={onSelect}
+                    />
+                </MenuWrapper>
+            )}
+        </div>
+    )
+}
+
+Autocomplete.defaultProps = {
+    dataTest: 'dhis2-uicore-select',
+}
+
+Autocomplete.propTypes = {
+    dataTest: propTypes.string,
+    inputWidth: propTypes.number,
+    maxHeight: propTypes.string,
+    placeholder: propTypes.string,
+    searchResults: propTypes.array,
+    value: propTypes.string,
+    onChange: propTypes.func,
+    onClose: propTypes.func,
+    onSearch: propTypes.func,
+}
--- packages/widgets/src/SharingDialog/sharingConstants.js
+++ packages/widgets/src/SharingDialog/sharingConstants.js
@@ -1,26 +1,26 @@
-import i18n from "@dhis2/d2-i18n";
+import i18n from '@dhis2/d2-i18n'
 
-export const ACCESS_NONE = "ACCESS_NONE";
-export const ACCESS_VIEW_ONLY = "ACCESS_VIEW_ONLY";
-export const ACCESS_VIEW_AND_EDIT = "ACCESS_VIEW_AND_EDIT";
+export const ACCESS_NONE = 'ACCESS_NONE'
+export const ACCESS_VIEW_ONLY = 'ACCESS_VIEW_ONLY'
+export const ACCESS_VIEW_AND_EDIT = 'ACCESS_VIEW_AND_EDIT'
 
 const noAccess = i18n.t('No access')
 export const accessStrings = {
-  [ACCESS_NONE]: {
-    publicDescription: noAccess,
-    description: noAccess,
-    option: noAccess
-  },
-  [ACCESS_VIEW_ONLY]: {
-    publicDescription: i18n.t('Anyone logged in can find and view'),
-    description: i18n.t('Can find and view'),
-    option: i18n.t('View only')
-  },
-  [ACCESS_VIEW_AND_EDIT]: {
-    publicDescription: i18n.t('Anyone logged in can find, edit, and view'),
-    description: i18n.t('Can find, edit, and view'),
-    option: i18n.t('Edit and view')
-  }
+    [ACCESS_NONE]: {
+        publicDescription: noAccess,
+        description: noAccess,
+        option: noAccess,
+    },
+    [ACCESS_VIEW_ONLY]: {
+        publicDescription: i18n.t('Anyone logged in can find and view'),
+        description: i18n.t('Can find and view'),
+        option: i18n.t('View only'),
+    },
+    [ACCESS_VIEW_AND_EDIT]: {
+        publicDescription: i18n.t('Anyone logged in can find, edit, and view'),
+        description: i18n.t('Can find, edit, and view'),
+        option: i18n.t('Edit and view'),
+    },
 }
 
 export const SHARE_TARGET_EXTERNAL = 'SHARE_TARGET_EXTERNAL'
@@ -28,25 +28,28 @@ export const SHARE_TARGET_PUBLIC = 'SHARE_TARGET_PUBLIC'
 export const SHARE_TARGET_USER = 'SHARE_TARGET_USER'
 export const SHARE_TARGET_GROUP = 'SHARE_TARGET_GROUP'
 
-export const isPermanentTarget = target => [SHARE_TARGET_EXTERNAL, SHARE_TARGET_PUBLIC].includes(target)
+export const isPermanentTarget = target =>
+    [SHARE_TARGET_EXTERNAL, SHARE_TARGET_PUBLIC].includes(target)
 
 export const defaultSharingSettings = {
     external: ACCESS_NONE,
     public: ACCESS_VIEW_ONLY,
-    groups: [],
-    users: []
+    groups: {},
+    users: {},
 }
 
 export const sharingSettingsAreEqual = (a, b) => {
-    const aGroups = Object.entries(a.groups).filter(([_, access]) => !!access)
-    const aUsers = Object.entries(a.users).filter(([_, access]) => !!access)
-    const bGroups = Object.entries(b.groups).filter(([_, access]) => !!access)
-    const bUsers = Object.entries(b.users).filter(([_, access]) => !!access)
-
-    return a.external === b.external &&
+    const aGroups = Object.values(a.groups).filter(({ access }) => !!access)
+    const aUsers = Object.values(a.users).filter(({ access }) => !!access)
+    const bGroups = Object.values(b.groups).filter(({ access }) => !!access)
+    const bUsers = Object.values(b.users).filter(({ access }) => !!access)
+    console.log('a U', aUsers, 'b U', bUsers, 'a G', aGroups, 'b G', bGroups)
+    return (
+        a.external === b.external &&
         a.public === b.public &&
         aGroups.length === bGroups.length &&
-        aGroups.every(([id, access]) => b.groups[id] === access) &&
+        aGroups.every(({ id, access }) => b.groups[id] === access) &&
         aUsers.length === bUsers.length &&
-        aUsers.every(([id, access]) => b.users[id] === access)
+        aUsers.every(({ id, access }) => b.users[id] === access)
+    )
 }
