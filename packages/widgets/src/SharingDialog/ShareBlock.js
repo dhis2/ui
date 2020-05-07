00f85049b4ff904d4ae3c862be44f594b196bbcf
Edoardo Sabadelli <edoardo@dhis2.org>
2020-05-07 12:44:10 +0200

    feat: search as you type for user/groups
    WIP

--- packages/widgets/src/SharingDialog/ShareBlock.js
+++ packages/widgets/src/SharingDialog/ShareBlock.js
@@ -1,21 +1,97 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import PropTypes from '@dhis2/prop-types'
 
 import i18n from '@dhis2/d2-i18n'
+import { useDataQuery } from '@dhis2/app-runtime'
 
 import { Button } from '@dhis2/ui-core'
-import { InputField } from '../'
+import { Autocomplete } from './Autocomplete/Autocomplete'
 import { AccessSelect } from './AccessSelect'
 import { shareBlockStyles } from './SharingDialog.styles'
+
+import { debounce } from './helpers'
+
+const query = {
+    usersAndGroups: {
+        resource: 'sharing/search',
+        params: ({ search }) => ({
+            key: search,
+        }),
+    },
+}
 
 export const ShareBlock = ({ onAdd }) => {
     const [userOrGroup, setUserOrGroup] = useState(undefined)
     const [access, setAccess] = useState(undefined)
+    const [usersAndGroups, setUsersAndGroups] = useState({})
+    const [searchResults, setSearchResults] = useState([])
+
+    const { data, error, refetch } = useDataQuery(query, {
+        lazy: true,
+    })
+
+    const addType = type => result => ({ ...result, type })
+
+    useEffect(() => {
+        if (data) {
+            setSearchResults(
+                data.usersAndGroups.users.concat(data.usersAndGroups.userGroups)
+            )
+
+            setUsersAndGroups(
+                data.usersAndGroups.users
+                    .map(addType('user'))
+                    .concat(
+                        data.usersAndGroups.userGroups.map(addType('group'))
+                    )
+                    .reduce((result, obj) => {
+                        result[obj.id] = obj
+
+                        return result
+                    }, {})
+            )
+        }
+    }, [data])
+
+    const fetchData = debounce(text => {
+        console.log('text', text)
+        refetch({ search: text })
+    }, 500)
+
+    const onSearch = text => {
+        setUserOrGroup({ name: text })
+
+        if (text.length) {
+            fetchData(text)
+        } else {
+            setSearchResults([])
+        }
+
+        if (error) {
+            console.log('onSearch error', error)
+        }
+    }
+
+    const onClose = () => {
+        setSearchResults([])
+        setUsersAndGroups({})
+    }
+
+    const onChange = id => {
+        setUserOrGroup(usersAndGroups[id])
+
+        onClose()
+    }
 
     const onSubmit = e => {
         e.preventDefault()
-        onAdd({ type: 'user', id: userOrGroup, access })
+        onAdd({
+            type: userOrGroup.type,
+            id: userOrGroup.id,
+            name: userOrGroup.displayName || userOrGroup.name,
+            access,
+        })
         setUserOrGroup(undefined)
         setAccess(undefined)
     }
@@ -25,16 +101,23 @@ export const ShareBlock = ({ onAdd }) => {
             <style jsx>{shareBlockStyles}</style>
             <p>{i18n.t('Share with users and groups')}</p>
             <form onSubmit={onSubmit} className="sharing-inputs">
-                <InputField
+                <Autocomplete
                     placeholder={i18n.t('Search for user, group or role')}
                     inputWidth="400px"
-                    value={userOrGroup}
-                    onChange={({ value }) => setUserOrGroup(value)}
+                    value={userOrGroup?.name}
+                    searchResults={searchResults}
+                    onClose={onClose}
+                    onChange={onChange}
+                    onSearch={onSearch}
                 />
                 <div className="select-wrap">
                     <AccessSelect access={access} onChange={setAccess} />
                 </div>
-                <Button type="submit" large disabled={!userOrGroup || !access}>
+                <Button
+                    type="submit"
+                    large
+                    disabled={!userOrGroup?.id || !access}
+                >
                     {i18n.t('Share')}
                 </Button>
             </form>
