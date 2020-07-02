05576c2a4c9d96eaefac90fab5de7ed8912dad8e
ismay <ismay@ismaywolff.nl>
2020-07-02 11:17:35 +0200

    fix(switchfield): use correct prop-type for value prop

--- packages/widgets/src/SwitchField/SwitchField.js
+++ packages/widgets/src/SwitchField/SwitchField.js
@@ -133,7 +133,7 @@ SwitchField.propTypes = {
     tabIndex: propTypes.string,
     valid: sharedPropTypes.statusPropType,
     validationText: propTypes.string,
-    value: propTypes.oneOf([propTypes.string, propTypes.bool]),
+    value: propTypes.oneOfType([propTypes.string, propTypes.bool]),
     warning: sharedPropTypes.statusPropType,
     onBlur: propTypes.func,
     onChange: propTypes.func,
