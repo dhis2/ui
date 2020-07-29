aad40f037ae88491d8768073afb2391a881aeaf8
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-07-29 16:27:27 +0200

    test(transfer): cover loading lists with cypress tests

---
+++ packages/widgets/src/Transfer/__e2e__/loading_lists.stories.e2e.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { Transfer } from '../Transfer'
+import { options } from './common/options'
+
+export default { title: 'Transfer Loading Lists' }
+
+export const LoadingSource = () => (
+    <Transfer
+        loadingSource
+        selected={[]}
+        onChange={() => null}
+        options={options}
+    />
+)
+
+export const LoadingPicked = () => (
+    <Transfer
+        loadingPicked
+        selected={[]}
+        onChange={() => null}
+        options={options}
+    />
+)
+
+export const NotLoadingSource = () => (
+    <Transfer selected={[]} onChange={() => null} options={options} />
+)
+
+export const NotLoadingPicked = () => (
+    <Transfer selected={[]} onChange={() => null} options={options} />
+)
---
+++ packages/widgets/src/Transfer/features/loading_lists/index.js
@@ -0,0 +1,43 @@
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+
+Given('the source list is loading', () => {
+    cy.visitStory('Transfer Loading Lists', 'Loading Source')
+    cy.wrap('source').as('listType')
+})
+
+Given('the picked list is loading', () => {
+    cy.visitStory('Transfer Loading Lists', 'Loading Picked')
+    cy.wrap('picked').as('listType')
+})
+
+Given('the source list is not loading', () => {
+    cy.visitStory('Transfer Loading Lists', 'Not Loading Source')
+    cy.wrap('source').as('listType')
+})
+
+Given('the picked list is not loading', () => {
+    cy.visitStory('Transfer Loading Lists', 'Not Loading Picked')
+    cy.wrap('picked').as('listType')
+})
+
+Then('the loading indicator should be shown', () => {
+    cy.get('@listType').then(listType => {
+        const listSelector =
+            listType === 'source'
+                ? '{transfer-leftside}'
+                : '{transfer-rightside}'
+
+        cy.get(`${listSelector} .loading`).should('exist')
+    })
+})
+
+Then('the loading indicator should not be shown', () => {
+    cy.get('@listType').then(listType => {
+        const listSelector =
+            listType === 'source'
+                ? '{transfer-leftside}'
+                : '{transfer-rightside}'
+
+        cy.get(`${listSelector} .loading`).should('not.exist')
+    })
+})
