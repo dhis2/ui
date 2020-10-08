a20553b27b33870f9f07854bf7fb6eecfcd9d9d4
ismay <ismay@ismaywolff.nl>
2020-10-08 17:35:38 +0200

    ci(icons): update config

--- packages/icons/svgr.config.js
+++ packages/icons/svgr.config.js
@@ -2,10 +2,25 @@ const indexTemplate = require('./templates/index-template.js')
 const template = require('./templates/template.js')
 
 module.exports = {
+    // This controls how the index.js file is generated
     indexTemplate,
+    // This controls how the svgs are transformed to jsx
     template,
+
+    // Compress svgs
     svgo: true,
     svgoConfig: {
-        plugins: [{ removeViewBox: false }],
+        plugins: [
+            {
+                removeViewBox: false,
+                mergePaths: true,
+            },
+        ],
+    },
+
+    // Replace hardcoded fills on paths, so that it can be overridden
+    replaceAttrValues: {
+        // For example, say our svg paths use red by default
+        red: 'inherit',
     },
 }
