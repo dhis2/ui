b3c4a8df194770bd9a1b79f9a4b75d117db2cbf4
Edoardo Sabadelli <edoardo@dhis2.org>
2020-10-12 16:05:15 +0200

    fix: implement disabled access select (external/public)
    These are controlled by the allowExternalAccess/allowPublicAccess.
    Use ui table components.

--- packages/widgets/src/SharingDialog/SharingListItem.js
+++ packages/widgets/src/SharingDialog/SharingListItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import PropTypes from '@dhis2/prop-types'
 
+import i18n from '@dhis2/d2-i18n'
+import { TableRow, TableCell } from '@dhis2/ui'
+
 import { sharingListItemStyles } from './SharingDialog.styles'
 import { IconExternal } from './icons/IconExternal'
-import { IconDelete } from './icons/IconDelete'
 import {
     SHARE_TARGET_PUBLIC,
-    accessStrings,
     isPermanentTarget,
+    accessStrings,
 } from './sharingConstants'
 import { AccessSelect } from './AccessSelect'
 
@@ -15,41 +17,45 @@ export const SharingListItem = ({
     name,
     target,
     access,
+    accessOptions,
+    disabled,
     onChange,
     onRemove,
 }) => (
-    <tr>
+    <TableRow>
         <style jsx>{sharingListItemStyles}</style>
-        <td className="icon">
-            <IconExternal />
-        </td>
-        <td className="details">
-            <p className="share-entity">{name}</p>
-            <p className="share-context">
-                {target === SHARE_TARGET_PUBLIC
-                    ? accessStrings[access]?.publicDescription
-                    : accessStrings[access]?.description}
-            </p>
-        </td>
-        <td>
-            <AccessSelect
-                access={access}
-                showNoAccessOption={isPermanentTarget(target)}
-                onChange={onChange}
-            />
-        </td>
-        <td className="share-delete">
-            {!isPermanentTarget(target) && (
-                <div onClick={onRemove}>
-                    <IconDelete />
+        <TableCell>
+            <div className="share-details">
+                <IconExternal className="icon" />
+                <div>
+                    <p className="share-entity">{name}</p>
+                    <p className="share-context">
+                        {target === SHARE_TARGET_PUBLIC
+                            ? accessStrings[access]?.publicDescription
+                            : accessStrings[access]?.description}
+                    </p>
                 </div>
-            )}
-        </td>
-    </tr>
+            </div>
+        </TableCell>
+        <TableCell>
+            <AccessSelect
+                prefix={i18n.t('Access')}
+                access={access}
+                accessOptions={accessOptions}
+                disabled={disabled}
+                showRemoveOption={!isPermanentTarget(target)}
+                onChange={selected =>
+                    selected === 'remove' ? onRemove() : onChange(selected)
+                }
+            />
+        </TableCell>
+    </TableRow>
 )
 
 SharingListItem.propTypes = {
     access: PropTypes.string,
+    accessOptions: PropTypes.array,
+    disabled: PropTypes.bool,
     name: PropTypes.string,
     target: PropTypes.string,
     onChange: PropTypes.func,
