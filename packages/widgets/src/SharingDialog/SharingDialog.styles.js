46b8120e9909acf32487457064a56c046ed93d88
Edoardo Sabadelli <edoardo@dhis2.org>
2020-10-12 16:09:42 +0200

    chore: cleanup styles

--- packages/widgets/src/SharingDialog/SharingDialog.styles.js
+++ packages/widgets/src/SharingDialog/SharingDialog.styles.js
@@ -2,70 +2,62 @@ import css from 'styled-jsx/css'
 
 import { colors } from '@dhis2/ui-constants'
 
-export const shareBlockStyles = css`
-  .share-block {
-    background-color: ${colors.grey100};
-    padding: 8px 16px;
-    border-radius: 5px;
-  }
-
-  .sharing-inputs {
-    display: flex;
-    width: 100%;
-  }
-
-  .select-wrap {
-    width: 172px;
-  }
+export const sharingCommonStyles = css`
+    .sharing-subtitle {
+        color: ${colors.grey700};
+        font-size: 15px;
+        font-weight: 500;
+        margin: 0;
+    }
 `
 
-export const sharingListStyles = css`
-  table {
-    width: 100%;
-  }
+export const shareBlockStyles = css`
+    .share-block {
+        background-color: ${colors.grey100};
+        margin-bottom: 31px;
+        padding: 16px;
+        border-radius: 5px;
+    }
+
+    .sharing-inputs {
+        display: flex;
+        align-items: flex-end;
+        width: 100%;
+    }
+
+    .select-wrap {
+        width: 172px;
+    }
 `
 
 export const sharingListItemStyles = css`
-  td {
-    border-bottom: 1px solid ${colors.grey300};
-  }
+    .icon {
+        width: 56px;
+        text-align: center;
+    }
 
-  .icon {
-    width: 56px;
-    text-align: center;
-  }
+    div.share-details {
+        display: flex;
+    }
 
-  .details {
-    padding: 8px 0px;
-  }
+    p.share-entity {
+        font-size: 16px;
+        font-weight: 500;
+        color: ${colors.grey900};
+        margin-bottom: 4px;
+        margin-top: 0px;
+    }
 
-  p.share-entity {
-    font-size: 16px;
-    margin-bottom: 4px;
-    margin-top: 0px;
-  }
-
-  p.share-context {
-    font-size: 14px;
-    color: ${colors.grey700};
-    margin-top: 4px;
-    margin-bottom: 8px;
-  }
-
-  td.share-delete {
-      width: 32px;
-  }
-  td.share-delete > div {
-      cursor: pointer;
-  }
-
-  span.heavy {
-    font-weight: 600;
-  }
+    p.share-context {
+        font-size: 14px;
+        color: ${colors.grey700};
+        margin-top: 4px;
+        margin-bottom: 8px;
+    }
 `
 
 export const accessSelectStyles = css`
-  .share-select {
-      width: 150px;
-  }
+    .share-select {
+        width: 150px;
+    }
 `
