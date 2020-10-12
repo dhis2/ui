ca0e45947c68db91fc1bab2d1fceeb1929d05a36
Edoardo Sabadelli <edoardo@dhis2.org>
2020-10-12 16:08:54 +0200

    chore: remove debug console logs

--- packages/widgets/src/SharingDialog/helpers.js
+++ packages/widgets/src/SharingDialog/helpers.js
@@ -2,12 +2,10 @@ export const debounce = function(f, ms) {
     let timeout
 
     return function(...args) {
-        console.log('args', args, timeout)
         if (timeout) {
             clearTimeout(timeout)
         }
         timeout = setTimeout(function() {
-            console.log('call and reset')
             timeout = undefined
             f(...args)
         }, ms)
