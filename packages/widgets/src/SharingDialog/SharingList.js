99c99e5a4e74f02d610afd0a3d3c82360d7e20a9
Edoardo Sabadelli <edoardo@dhis2.org>
2020-10-13 16:22:16 +0200

    refactor: replace Table components with divs and fix styling
    The information presented is not data that requires tabular display,
    more like a simple list of selections formatted to align the selects.
    This should simplify the implementation of the design, especially for
    implementing the scrolling of the list, while the rest stays fixed.

--- packages/widgets/src/SharingDialog/SharingList.js
+++ packages/widgets/src/SharingDialog/SharingList.js
@@ -2,16 +2,9 @@ import React from 'react'
 
 import PropTypes from '@dhis2/prop-types'
 import i18n from '@dhis2/d2-i18n'
-import {
-    Divider,
-    Table,
-    TableHead,
-    TableRowHead,
-    TableCellHead,
-    TableBody,
-} from '@dhis2/ui'
+import { Divider } from '@dhis2/ui'
 
-import { sharingCommonStyles } from './SharingDialog.styles'
+import { sharingCommonStyles, sharingListStyles } from './SharingDialog.styles'
 import {
     SHARE_TARGET_PUBLIC,
     SHARE_TARGET_EXTERNAL,
@@ -25,46 +18,64 @@ import {
 import { SharingListItem } from './SharingListItem'
 
 export const SharingList = ({ sharingSettings, onChange, onRemove }) => (
-    <div>
+    <>
         <style jsx>{sharingCommonStyles}</style>
+        <style jsx>{sharingListStyles}</style>
         <p className="sharing-subtitle">
             {i18n.t('Users, groups and roles that currently have access')}
         </p>
         <Divider />
-        <Table suppressZebraStriping>
-            <TableHead>
-                <TableRowHead>
-                    <TableCellHead>
-                        {i18n.t('User / Group / Role')}
-                    </TableCellHead>
-                    <TableCellHead>{i18n.t('Access level')}</TableCellHead>
-                </TableRowHead>
-            </TableHead>
-            <TableBody>
-                <SharingListItem
-                    name={i18n.t('External')}
-                    target={SHARE_TARGET_EXTERNAL}
-                    access={sharingSettings.external}
-                    accessOptions={[ACCESS_NONE, ACCESS_VIEW_ONLY]}
-                    disabled={!sharingSettings.allowExternal}
-                    onChange={access => onChange({ type: 'external', access })}
-                />
-                <SharingListItem
-                    name={i18n.t('Public')}
-                    target={SHARE_TARGET_PUBLIC}
-                    access={sharingSettings.public}
-                    accessOptions={Object.keys(accessStrings)}
-                    disabled={!sharingSettings.allowPublic}
-                    onChange={newAccess =>
-                        onChange({ type: 'public', access: newAccess })
-                    }
-                />
-                {Object.values(sharingSettings.groups).map(
-                    ({ id, name, access }) => (
+        <div className="sharing-headers">
+            <div className="sharing-header-1">
+                {i18n.t('User / Group / Role')}
+            </div>
+            <div className="sharing-header-2">{i18n.t('Access level')}</div>
+        </div>
+        <div className="sharing-list">
+            <SharingListItem
+                name={i18n.t('External users')}
+                target={SHARE_TARGET_EXTERNAL}
+                access={sharingSettings.external}
+                accessOptions={[ACCESS_NONE, ACCESS_VIEW_ONLY]}
+                disabled={!sharingSettings.allowExternal}
+                onChange={access => onChange({ type: 'external', access })}
+            />
+            <SharingListItem
+                name={i18n.t('All users')}
+                target={SHARE_TARGET_PUBLIC}
+                access={sharingSettings.public}
+                accessOptions={Object.keys(accessStrings)}
+                disabled={!sharingSettings.allowPublic}
+                onChange={newAccess =>
+                    onChange({ type: 'public', access: newAccess })
+                }
+            />
+            {Object.values(sharingSettings.groups).map(
+                ({ id, name, access }) => (
+                    <SharingListItem
+                        key={id}
+                        name={name}
+                        target={SHARE_TARGET_GROUP}
+                        access={access}
+                        accessOptions={[ACCESS_VIEW_ONLY, ACCESS_VIEW_AND_EDIT]}
+                        onChange={newAccess =>
+                            onChange({
+                                type: 'group',
+                                id,
+                                access: newAccess,
+                            })
+                        }
+                        onRemove={() => onRemove({ type: 'group', id })}
+                    />
+                )
+            )}
+            {Object.values(sharingSettings.users).map(
+                ({ id, name, access }) =>
+                    access && (
                         <SharingListItem
                             key={id}
                             name={name}
-                            target={SHARE_TARGET_GROUP}
+                            target={SHARE_TARGET_USER}
                             access={access}
                             accessOptions={[
                                 ACCESS_VIEW_ONLY,
@@ -72,41 +83,17 @@ export const SharingList = ({ sharingSettings, onChange, onRemove }) => (
                             ]}
                             onChange={newAccess =>
                                 onChange({
-                                    type: 'group',
+                                    type: 'user',
                                     id,
                                     access: newAccess,
                                 })
                             }
-                            onRemove={() => onRemove({ type: 'group', id })}
+                            onRemove={() => onRemove({ type: 'user', id })}
                         />
                     )
-                )}
-                {Object.values(sharingSettings.users).map(
-                    ({ id, name, access }) =>
-                        access && (
-                            <SharingListItem
-                                key={id}
-                                name={name}
-                                target={SHARE_TARGET_USER}
-                                access={access}
-                                accessOptions={[
-                                    ACCESS_VIEW_ONLY,
-                                    ACCESS_VIEW_AND_EDIT,
-                                ]}
-                                onChange={newAccess =>
-                                    onChange({
-                                        type: 'user',
-                                        id,
-                                        access: newAccess,
-                                    })
-                                }
-                                onRemove={() => onRemove({ type: 'user', id })}
-                            />
-                        )
-                )}
-            </TableBody>
-        </Table>
-    </div>
+            )}
+        </div>
+    </>
 )
 
 SharingList.propTypes = {
