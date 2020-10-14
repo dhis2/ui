5f5789cd556c5741314faaae935a67f947a7fc40
Edoardo Sabadelli <edoardo@dhis2.org>
2020-10-14 11:01:28 +0200

    refactor: use name initials icon for users

---
+++ packages/widgets/src/SharingDialog/icons/Avatar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import PropTypes from '@dhis2/prop-types'
+
+import { avatarStyles } from '../SharingDialog.styles'
+
+export const Avatar = ({ name } = { name: 'USER' }) => {
+    const nameParts = name.split(' ')
+
+    let initials = nameParts.shift().charAt(0)
+
+    if (nameParts.length) {
+        initials += nameParts.pop().charAt(0)
+    }
+
+    return (
+        <div className="share-details-avatar">
+            <style jsx>{avatarStyles}</style>
+            <p>{initials}</p>
+        </div>
+    )
+}
+
+Avatar.propTypes = {
+    name: PropTypes.string,
+}
