571bfa92938f8c68d400fb2d4e2a2faf3e9e8156
ismay <ismay@ismaywolff.nl>
2020-12-15 18:03:34 +0100

    test(storybook): enable code that still allows storybook to function

--- packages/widgets/src/SharingDialog/ShareBlock.js
+++ packages/widgets/src/SharingDialog/ShareBlock.js
@@ -1,137 +1,14 @@
 import React, { useEffect, useState } from 'react'
-
 import PropTypes from '@dhis2/prop-types'
-
 import i18n from '@dhis2/d2-i18n'
 import { useDataQuery } from '@dhis2/app-runtime'
 import { Button, Divider } from '@dhis2/ui-core'
+// import { ACCESS_VIEW_ONLY, ACCESS_VIEW_AND_EDIT } from './sharingConstants.js'
+// import { Autocomplete } from './Autocomplete/Autocomplete.js'
+// import { AccessSelect } from './AccessSelect.js'
+// import { sharingCommonStyles, shareBlockStyles } from './SharingDialog.styles'
+// import { debounce } from './helpers'
 
-import { ACCESS_VIEW_ONLY, ACCESS_VIEW_AND_EDIT } from './sharingConstants.js'
-import { Autocomplete } from './Autocomplete/Autocomplete.js'
-import { AccessSelect } from './AccessSelect.js'
-import { sharingCommonStyles, shareBlockStyles } from './SharingDialog.styles'
-
-import { debounce } from './helpers'
-
-const query = {
-    usersAndGroups: {
-        resource: 'sharing/search',
-        params: ({ search }) => ({
-            key: search,
-        }),
-    },
-}
-
-export const ShareBlock = ({ onAdd }) => {
-    const [userOrGroup, setUserOrGroup] = useState(undefined)
-    const [access, setAccess] = useState(undefined)
-    const [usersAndGroups, setUsersAndGroups] = useState({})
-    const [searchResults, setSearchResults] = useState([])
-
-    const { data, error, refetch } = useDataQuery(query, {
-        lazy: true,
-    })
-
-    const addType = type => result => ({ ...result, type })
-
-    useEffect(() => {
-        if (data) {
-            setSearchResults(
-                data.usersAndGroups.users.concat(data.usersAndGroups.userGroups)
-            )
-
-            setUsersAndGroups(
-                data.usersAndGroups.users
-                    .map(addType('user'))
-                    .concat(
-                        data.usersAndGroups.userGroups.map(addType('group'))
-                    )
-                    .reduce((result, obj) => {
-                        result[obj.id] = obj
-
-                        return result
-                    }, {})
-            )
-        }
-    }, [data])
-
-    const fetchData = debounce(text => {
-        refetch({ search: text })
-    }, 500)
-
-    const onSearch = text => {
-        setUserOrGroup({ name: text })
-
-        if (text.length) {
-            fetchData(text)
-        } else {
-            setSearchResults([])
-        }
-
-        if (error) {
-            console.error('onSearch error', error)
-        }
-    }
-
-    const onClose = () => {
-        setSearchResults([])
-        setUsersAndGroups({})
-    }
-
-    const onChange = id => {
-        setUserOrGroup(usersAndGroups[id])
-
-        onClose()
-    }
-
-    const onSubmit = e => {
-        e.preventDefault()
-
-        onAdd({
-            type: userOrGroup.type,
-            id: userOrGroup.id,
-            name: userOrGroup.displayName || userOrGroup.name,
-            access,
-        })
-
-        setUserOrGroup(undefined)
-        setAccess(undefined)
-    }
-
-    return (
-        <>
-            <style jsx>{sharingCommonStyles}</style>
-            <style jsx>{shareBlockStyles}</style>
-            <p className="sharing-subtitle">
-                {i18n.t('Give access to a user, group or role')}
-            </p>
-            <Divider />
-            <form onSubmit={onSubmit} className="share-block sharing-inputs">
-                <Autocomplete
-                    placeholder={i18n.t('Search')}
-                    label={i18n.t('User, group or role')}
-                    inputWidth="400px"
-                    value={userOrGroup?.name}
-                    searchResults={searchResults}
-                    onClose={onClose}
-                    onChange={onChange}
-                    onSearch={onSearch}
-                />
-                <AccessSelect
-                    label={i18n.t('Access level')}
-                    placeholder={i18n.t('Select a level')}
-                    access={access}
-                    accessOptions={[ACCESS_VIEW_ONLY, ACCESS_VIEW_AND_EDIT]}
-                    onChange={setAccess}
-                />
-                <Button type="submit" disabled={!userOrGroup?.id || !access}>
-                    {i18n.t('Give access')}
-                </Button>
-            </form>
-        </>
-    )
-}
-
-ShareBlock.propTypes = {
-    onAdd: PropTypes.func,
+export const ShareBlock = () => {
+    return null
 }
--- packages/widgets/src/SharingDialog/SharingList.js
+++ packages/widgets/src/SharingDialog/SharingList.js
@@ -1,103 +1,18 @@
 import React from 'react'
-
 import PropTypes from '@dhis2/prop-types'
 import i18n from '@dhis2/d2-i18n'
-import { Divider } from '@dhis2/ui'
+// import { Divider } from '@dhis2/ui'
+// import { sharingCommonStyles, sharingListStyles } from './SharingDialog.styles'
+// import {
+//     SHARE_TARGET_PUBLIC,
+//     SHARE_TARGET_EXTERNAL,
+//     SHARE_TARGET_GROUP,
+//     SHARE_TARGET_USER,
+//     accessStrings,
+//     ACCESS_NONE,
+//     ACCESS_VIEW_ONLY,
+//     ACCESS_VIEW_AND_EDIT,
+// } from './sharingConstants'
+// import { SharingListItem } from './SharingListItem'
 
-import { sharingCommonStyles, sharingListStyles } from './SharingDialog.styles'
-import {
-    SHARE_TARGET_PUBLIC,
-    SHARE_TARGET_EXTERNAL,
-    SHARE_TARGET_GROUP,
-    SHARE_TARGET_USER,
-    accessStrings,
-    ACCESS_NONE,
-    ACCESS_VIEW_ONLY,
-    ACCESS_VIEW_AND_EDIT,
-} from './sharingConstants'
-import { SharingListItem } from './SharingListItem'
-
-export const SharingList = ({ sharingSettings, onChange, onRemove }) => (
-    <>
-        <style jsx>{sharingCommonStyles}</style>
-        <style jsx>{sharingListStyles}</style>
-        <p className="sharing-subtitle">
-            {i18n.t('Users, groups and roles that currently have access')}
-        </p>
-        <Divider />
-        <div className="sharing-headers">
-            <div className="sharing-header-1">
-                {i18n.t('User / Group / Role')}
-            </div>
-            <div className="sharing-header-2">{i18n.t('Access level')}</div>
-        </div>
-        <div className="sharing-list">
-            <SharingListItem
-                name={i18n.t('External users')}
-                target={SHARE_TARGET_EXTERNAL}
-                access={sharingSettings.external}
-                accessOptions={[ACCESS_NONE, ACCESS_VIEW_ONLY]}
-                disabled={!sharingSettings.allowExternal}
-                onChange={access => onChange({ type: 'external', access })}
-            />
-            <SharingListItem
-                name={i18n.t('All users')}
-                target={SHARE_TARGET_PUBLIC}
-                access={sharingSettings.public}
-                accessOptions={Object.keys(accessStrings)}
-                disabled={!sharingSettings.allowPublic}
-                onChange={newAccess =>
-                    onChange({ type: 'public', access: newAccess })
-                }
-            />
-            {Object.values(sharingSettings.groups).map(
-                ({ id, name, access }) => (
-                    <SharingListItem
-                        key={id}
-                        name={name}
-                        target={SHARE_TARGET_GROUP}
-                        access={access}
-                        accessOptions={[ACCESS_VIEW_ONLY, ACCESS_VIEW_AND_EDIT]}
-                        onChange={newAccess =>
-                            onChange({
-                                type: 'group',
-                                id,
-                                access: newAccess,
-                            })
-                        }
-                        onRemove={() => onRemove({ type: 'group', id })}
-                    />
-                )
-            )}
-            {Object.values(sharingSettings.users).map(
-                ({ id, name, access }) =>
-                    access && (
-                        <SharingListItem
-                            key={id}
-                            name={name}
-                            target={SHARE_TARGET_USER}
-                            access={access}
-                            accessOptions={[
-                                ACCESS_VIEW_ONLY,
-                                ACCESS_VIEW_AND_EDIT,
-                            ]}
-                            onChange={newAccess =>
-                                onChange({
-                                    type: 'user',
-                                    id,
-                                    access: newAccess,
-                                })
-                            }
-                            onRemove={() => onRemove({ type: 'user', id })}
-                        />
-                    )
-            )}
-        </div>
-    </>
-)
-
-SharingList.propTypes = {
-    sharingSettings: PropTypes.object,
-    onChange: PropTypes.func,
-    onRemove: PropTypes.func,
-}
+export const SharingList = ({ sharingSettings, onChange, onRemove }) => null
