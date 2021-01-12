374a72c9ef409a30f1fc55dcb25b6a0c4c5d7ce2
Hendrik de Graaf <hendrik@dhis2.org>
2021-01-12 20:52:56 +0100

    chore(cypress): create plugin to test parallel execution behaviour

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,4 +6,37 @@ const {
 module.exports = (on, config) => {
     chromeAllowXSiteCookies(on, config)
     cucumberPreprocessor(on, config)
+
+    const state = {
+        runStarts: 0,
+        runEnds: 0,
+        specStarts: 0,
+        specEnds: 0,
+    }
+
+    on('before:run', () => {
+        state.runStarts++
+        console.log(
+            `before:run event: incremented runStarts to ${state.runStarts}`
+        )
+    })
+
+    on('after:run', () => {
+        state.runEnds++
+        console.log(`before:run event: incremented runEnds to ${state.runEnds}`)
+    })
+
+    on('before:spec', ({ name }) => {
+        state.specStarts++
+        console.log(
+            `before:spec event: ${name} - incremented specStarts to ${state.specStarts}`
+        )
+    })
+
+    on('after:spec', ({ name }) => {
+        state.specEnds++
+        console.log(
+            `before:spec event: ${name} - incremented specEnds to ${state.specEnds}`
+        )
+    })
 }
