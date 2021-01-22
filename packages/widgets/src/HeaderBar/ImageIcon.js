aea92290fe2047f561184fcdb0622a9b41b872af
Médi-Rémi Hashim <mediremi@gmail.com>
2021-01-22 12:47:35 +0000

    fix: make ImageIcon compliant with design-system specs

--- packages/widgets/src/HeaderBar/ImageIcon.js
+++ packages/widgets/src/HeaderBar/ImageIcon.js
@@ -14,6 +14,8 @@ export const ImageIcon = ({ src, onClick, dataTestId }) => (
             img {
                 width: 100%;
                 height: 100%;
+                border-radius: 50%;
+                object-fit: cover;
             }
         `}</style>
     </>
