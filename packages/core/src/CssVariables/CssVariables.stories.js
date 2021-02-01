e254facb38230f9d0b554ab29212ed314674c332
Kai Vandivier <kpvandivier@gmail.com>
2021-02-01 15:51:35 +0100

    docs: description, args for CssVariables

--- packages/core/src/CssVariables/CssVariables.stories.js
+++ packages/core/src/CssVariables/CssVariables.stories.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import { CssVariables } from './CssVariables.js'
 
+const description = `
+A utility for adding DHIS2 theme variables to global CSS variables.
+
+\`\`\`js
+import { CssVariables } from '@dhis2/ui'
+\`\`\`
+`
+
 // eslint-disable-next-line react/prop-types
 const App = ({ children }) => <div>{children}</div>
 
 export default {
     title: 'Helpers/CSS Variables',
     component: CssVariables,
+    parameters: { docs: { description: { component: description } } },
 }
 
-export const Default = () => (
+export const AllVariables = args => (
     <App>
-        <CssVariables />
-
-        <p>By default no custom properties are inserted.</p>
-    </App>
-)
-
-export const All = () => (
-    <App>
-        <CssVariables colors theme layers spacers elevations />
+        <CssVariables {...args} />
 
         <p>
             The sections of the theme that should be inserted can be toggled
@@ -32,5 +33,20 @@ export const All = () => (
             </span>{' '}
             set by the CssVariables component.
         </p>
+    </App>
+)
+AllVariables.args = {
+    colors: true,
+    theme: true,
+    layers: true,
+    spacers: true,
+    elevations: true,
+}
+
+export const NoVariables = args => (
+    <App>
+        <CssVariables {...args} />
+
+        <p>By default no custom properties are inserted.</p>
     </App>
 )
