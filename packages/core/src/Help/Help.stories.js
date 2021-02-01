61167a3aafb341e196adaf9472c0a5b2749b7818
Kai Vandivier <kpvandivier@gmail.com>
2021-02-01 22:46:57 +0100

    docs: description, args, stories for Help

--- packages/core/src/Help/Help.stories.js
+++ packages/core/src/Help/Help.stories.js
@@ -1,27 +1,45 @@
+import { sharedPropTypes } from '@dhis2/ui-constants'
 import React from 'react'
 import { Help } from './Help.js'
+
+const description = `
+Small text for giving guiding information or feedback, especially for data entry instructions or form validation feedback.
+
+\`\`\`js
+import { Help } from '@dhis2/ui'
+\`\`\`
+`
 
 export default {
     title: 'Forms/Help',
     component: Help,
+    parameters: { docs: { description: { component: description } } },
+    argTypes: {
+        valid: { ...sharedPropTypes.statusArgType },
+        warning: { ...sharedPropTypes.statusArgType },
+        error: { ...sharedPropTypes.statusArgType },
+    },
+    args: { children: 'Allow me to be of assistance' },
 }
 
-export const Default = () => <Help>Allow me to be of assistance</Help>
+const Template = args => <Help {...args} />
 
-export const StatusWarning = () => (
-    <Help warning>Allow me to be of assistance</Help>
-)
+export const Default = Template.bind({})
+
+export const StatusWarning = Template.bind({})
+StatusWarning.args = { warning: true }
 StatusWarning.storyName = 'Status: Warning'
 
-export const StatusValid = () => <Help valid>Allow me to be of assistance</Help>
+export const StatusValid = Template.bind({})
+StatusValid.args = { valid: true }
 StatusValid.storyName = 'Status: Valid'
 
-export const StatusError = () => <Help error>Allow me to be of assistance</Help>
+export const StatusError = Template.bind({})
+StatusError.args = { error: true }
 StatusError.storyName = 'Status: Error'
 
-export const TextOverflow = () => (
+export const TextOverflow = args => (
     <div style={{ width: 200 }}>
-        <Help>I take up more space than my container</Help>
+        <Help {...args}>I take up more space than my container</Help>
     </div>
 )
-TextOverflow.storyName = 'Text overflow'
