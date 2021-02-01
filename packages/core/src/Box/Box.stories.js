8cd06dccd52d9fc445ade5f1644b1c87b038daea
Kai Vandivier <kpvandivier@gmail.com>
2021-02-01 10:08:31 +0100

    docs(storybook): add description, use args for stories

--- packages/core/src/Box/Box.stories.js
+++ packages/core/src/Box/Box.stories.js
@@ -1,35 +1,58 @@
 import React from 'react'
 import { Box } from './Box.js'
 
+const description = `
+A box for creating some layout on the page.
+
+\`\`\`js
+import { Box } from '@dhis2/ui'
+\`\`\`
+`
+
 export default {
     title: 'Layout/Box',
     component: Box,
+    parameters: { docs: { description: { component: description } } },
 }
 
-export const Default = () => <Box>I am a child in a Box.</Box>
+const Template = args => <Box {...args} />
 
-export const Height = () => <Box height="250px">I am a child in a Box.</Box>
+export const Default = Template.bind({})
+Default.args = { children: 'I am a child in a Box.' }
 
-export const MaxHeight = () => (
-    <Box maxHeight="250px">
+export const Height = Template.bind({})
+Height.args = {
+    ...Default.args,
+    height: '250px',
+}
+
+export const MaxHeight = Template.bind({})
+MaxHeight.args = {
+    children: (
         <p style={{ height: '500px' }}>I am a tall child in a low Box.</p>
-    </Box>
-)
+    ),
+    maxHeight: '250px',
+}
 
-export const MinHeight = () => (
-    <Box minHeight="50vh">I am a child in a Box.</Box>
-)
+export const MinHeight = Template.bind({})
+MinHeight.args = { ...Default.args, minHeight: '50vh' }
 
-export const Width = () => <Box width="250px">I am a child in a Box.</Box>
+export const Width = Template.bind({})
+Width.args = { ...Default.args, width: '250px' }
 
-export const MinWidth = () => <Box minWidth="50vh">I am a child in a Box.</Box>
+export const MinWidth = Template.bind({})
+MinWidth.args = { ...Default.args, minWidth: '50vh' }
 
-export const MaxWidth = () => <Box maxWidth="50vh">I am a child in a Box.</Box>
+export const MaxWidth = Template.bind({})
+MaxWidth.args = { ...Default.args, maxWidth: '50vh' }
 
-export const Overflow = () => (
-    <Box maxHeight="250px" overflow="scroll">
+export const Overflow = Template.bind({})
+Overflow.args = {
+    maxHeight: '250px',
+    overflow: 'scroll',
+    children: (
         <p style={{ height: '500px' }}>
             I am a tall child in a low Box, and my parent clips me
         </p>
-    </Box>
-)
+    ),
+}
