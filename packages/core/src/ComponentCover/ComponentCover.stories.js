f8f8e5cea022ac32e107aa879ce876662d98bc8a
Kai Vandivier <kpvandivier@gmail.com>
2021-02-01 15:30:34 +0100

    docs: description, template, args for ComponentCover

--- packages/core/src/ComponentCover/ComponentCover.stories.js
+++ packages/core/src/ComponentCover/ComponentCover.stories.js
@@ -2,62 +2,50 @@ import React from 'react'
 import { CircularLoader, CenteredContent } from '../index.js'
 import { ComponentCover } from './ComponentCover.js'
 
+const description = `
+Covers sibling components.  Useful for covering a component while it is loading.
+
+\`\`\`js
+import { ComponentCover } from '@dhis2/ui'
+\`\`\`
+`
+
 export default {
     title: 'Helpers/Component Cover',
     component: ComponentCover,
-    decorators: [
-        storyFn => (
-            <div>
-                {storyFn()}
-                <style jsx>{`
-                    div {
-                        width: 400px;
-                        height: 400px;
-                        position: relative;
-                        border: 1px dotted grey;
-                    }
-                `}</style>
-            </div>
-        ),
-    ],
+    parameters: { docs: { description: { component: description } } },
 }
 
-export const Default = () => (
-    <>
-        <ComponentCover />
+const Template = args => (
+    <div
+        style={{
+            width: '400px',
+            height: '200px',
+            position: 'relative',
+            border: '1px dotted grey',
+        }}
+    >
+        <ComponentCover {...args} />
 
         <h1>Text behind the cover</h1>
         <p>Lorem ipsum</p>
-    </>
+    </div>
 )
 
-export const Translucent = () => (
-    <>
-        <ComponentCover translucent />
+export const Default = Template.bind({})
 
-        <h1>Text behind the cover</h1>
-        <p>Lorem ipsum</p>
-    </>
-)
+export const Translucent = Template.bind({})
+Translucent.args = { translucent: true }
 
-export const WithClickHandler = () => (
-    <>
-        <ComponentCover onClick={() => alert('Cover was clicked')} />
+export const WithClickHandler = Template.bind({})
+WithClickHandler.args = { onClick: () => alert('Cover was clicked') }
 
-        <h1>Text behind the cover</h1>
-        <p>Lorem ipsum</p>
-    </>
-)
-
-export const WithCenteredContentCircularLoader = () => (
-    <>
-        <ComponentCover translucent>
-            <CenteredContent>
-                <CircularLoader />
-            </CenteredContent>
-        </ComponentCover>
-
-        <h1>Text behind the cover</h1>
-        <p>Lorem ipsum</p>
-    </>
-)
+export const WithCenteredContentCircularLoader = Template.bind({})
+WithCenteredContentCircularLoader.args = {
+    translucent: true,
+    children: (
+        <CenteredContent>
+            <CircularLoader />
+        </CenteredContent>
+    ),
+}
