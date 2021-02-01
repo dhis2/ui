c67cc3acd92e31e4ca903e20402b2f56ba73c1c1
Kai Vandivier <kpvandivier@gmail.com>
2021-02-01 15:35:35 +0100

    docs: description, args for CssReset

--- packages/core/src/CssReset/CssReset.stories.js
+++ packages/core/src/CssReset/CssReset.stories.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { CssReset } from './CssReset.js'
 
+const description = `
+A tool for adding a global normalization stylesheet into the DOM that applies DHIS2 styles.
+
+- https://github.com/necolas/normalize.css
+- https://www.paulirish.com/2012/box-sizing-border-box-ftw/
+
+\`\`\`js
+import { CssReset } from '@dhis2/ui'
+\`\`\`
+`
+
 // eslint-disable-next-line react/prop-types
 const App = ({ children }) => <div>{children}</div>
 
 export default {
     title: 'Helpers/CSS Reset',
     component: CssReset,
+    parameters: { docs: { description: { component: description } } },
 }
 
-export const Default = () => (
+export const Default = args => (
     <App>
-        <CssReset />
+        <CssReset {...args} />
 
         <p>
             The <code>CssReset</code> component injects a global normalization
@@ -19,9 +31,9 @@ export const Default = () => (
         </p>
 
         <p>
-            This also sets the <code>font-family</code> on the
-            <code>body</code> element to the DHIS2 font, which allows it to
-            trickle down to the components as well.
+            This also sets the <code>font-family</code> on the <code>body</code>{' '}
+            element to the DHIS2 font, which allows it to trickle down to the
+            components as well.
         </p>
 
         <p>
