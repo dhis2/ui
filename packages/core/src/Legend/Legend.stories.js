acbaacd1e6fa06c7b4eddc4983b9a558ef0c2cc7
Kai Vandivier <kpvandivier@gmail.com>
2021-02-02 13:12:57 +0100

    docs: description, template for Legend

--- packages/core/src/Legend/Legend.stories.js
+++ packages/core/src/Legend/Legend.stories.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Legend } from './Legend.js'
 
+const description = `
+Legends are to be used in a Field Set to describe the form fields.  They may indicate that the fields are required.
+
+See the [Field Set](../?path=/docs/forms-field-set-field-set--usage-example-a-radio-button-group-with-error-status) for a usage example.
+
+\`\`\`js
+import { Legend } from '@dhis2/ui'
+\`\`\`
+`
+
 export default {
     title: 'Forms/Field Set/Legend',
     component: Legend,
+    parameters: { docs: { description: { component: description } } },
 }
 
-export const Default = () => (
-    <Legend>I am wrapped in a legend which has some styling</Legend>
+const Template = args => (
+    <Legend {...args}>I am wrapped in a legend which has some styling</Legend>
 )
 
-export const Required = () => (
-    <Legend required>I am wrapped in a legend which has some styling</Legend>
-)
+export const Default = Template.bind({})
+
+export const Required = Template.bind({})
+Required.args = { required: true }
