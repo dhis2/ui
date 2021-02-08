aa3e64bdd260b901ecdf93beea6a4169ad12b439
Hendrik de Graaf <hendrik@dhis2.org>
2021-02-08 12:03:55 +0100

    chore(table): rename TableBox to TableScrollBox and tweak scroll styles

--- packages/core/src/Table/TableScrollBox.js
+++ packages/core/src/Table/TableScrollBox.js
@@ -7,23 +7,39 @@ import { Box } from '../Box/Box.js'
 
 const { className: tableClassName, styles } = css.resolve`
     div {
-        border-right: 1px solid ${colors.grey300};
+        border: 1px solid ${colors.grey300};
         box-sizing: border-box;
+    }
+    div > :global(table) :global(tr) :global(td:first-child),
+    div > :global(table) :global(tr) :global(th:first-child) {
+        border-left: none;
     }
     div > :global(table) :global(tr) :global(td:last-child),
     div > :global(table) :global(tr) :global(th:last-child) {
         border-right: none;
     }
+    div > :global(table) > :global(tr:first-child) :global(th),
+    div
+        > :global(table)
+        > :global(thead)
+        > :global(tr:first-child)
+        :global(th) {
+        border-top: none;
+    }
+    div > :global(table) > :global(tr:last-child) :global(td),
+    div > :global(table) > :global(tfoot) > :global(tr:last-child) :global(td) {
+        border-bottom: none;
+    }
 `
 
 /**
  * @module
- * @param {TableBox.PropTypes} props
+ * @param {TableScrollBox.PropTypes} props
  * @returns {React.Component}
- * @example import { TableBox } from '@dhis2/ui'
+ * @example import { TableScrollBox } from '@dhis2/ui'
  * @see Live demo: {@link /demo/?path=/story/table--static-layout|Storybook}
  */
-export const TableBox = ({ className, children, dataTest, ...props }) => (
+export const TableScrollBox = ({ className, children, dataTest, ...props }) => (
     <Box
         className={cx(className, tableClassName)}
         dataTest={dataTest}
@@ -34,7 +50,7 @@ export const TableBox = ({ className, children, dataTest, ...props }) => (
     </Box>
 )
 
-TableBox.propTypes = {
+TableScrollBox.propTypes = {
     dataTest: 'dhis2-uicore-tablebox',
 }
 
@@ -45,7 +61,7 @@ TableBox.propTypes = {
  * @prop {string} [className]
  * @prop {string} [dataTest=dhis2-uicore-tablebox]
  */
-TableBox.propTypes = {
+TableScrollBox.propTypes = {
     children: propTypes.node,
     className: propTypes.string,
     dataTest: propTypes.string,
