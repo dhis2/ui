4ef9005fd4baf5d7a58737d606ede2f61f9a6953
Kai Vandivier <kpvandivier@gmail.com>
2021-02-09 11:35:45 +0100

    docs: renovate stories for MultiSelect

--- packages/core/src/MultiSelect/MultiSelect.stories.js
+++ packages/core/src/MultiSelect/MultiSelect.stories.js
@@ -1,10 +1,30 @@
 import propTypes from '@dhis2/prop-types'
+import { sharedPropTypes } from '@dhis2/ui-constants'
 import React from 'react'
 import { MultiSelect, MultiSelectOption } from '../index.js'
 
-window.onChange = window.Cypress && window.Cypress.cy.stub()
-window.onFocus = window.Cypress && window.Cypress.cy.stub()
-window.onBlur = window.Cypress && window.Cypress.cy.stub()
+const description = `
+_**Note:** Due to demo limitations, only one representative example is rendered here. For more (interactive) examples, see individual stories in the 'Canvas' tab._
+
+Multiple selection allows the user to select more than one option from the list. Checkboxes are used to highlight the possibility of making more than one selection. Selected options are displayed as chips inside the input. Make sure to communicate clearly to the user if there is a limit to the number of selectable elements.
+
+Use multiple selection wherever the user needs to make more than one selection. If the multiple selection is complex, requires a defined order or needs to made from different groups, consider using a transfer component instead.
+
+Read more about using \`Select\` components at [Design System: Select](https://github.com/dhis2/design-system/blob/master/molecules/select.md).
+
+\`\`\`js
+import { MultiSelect, MultiSelectOption } from '@dhis2/ui'
+\`\`\`
+`
+
+const eventHandler = handlerName => (payload, event) => {
+    console.log(`${handlerName} payload`, payload)
+    console.log(`${handlerName} event`, event)
+}
+
+const onChange = eventHandler('onChange')
+const onFocus = eventHandler('onFocus')
+const onBlur = eventHandler('onBlur')
 
 const CustomMultiSelectOption = ({ label, onClick }) => (
     <div onClick={e => onClick({}, e)}>{label}</div>
@@ -15,53 +35,62 @@ CustomMultiSelectOption.propTypes = {
     onClick: propTypes.func,
 }
 
+const requiredIfArgType = {
+    table: { type: { summary: 'string' }, control: { type: 'string' } },
+}
+
 export default {
     title: 'Forms/Multi Select/Multi Select',
     component: MultiSelect,
+    parameters: {
+        docs: {
+            description: { component: description },
+            // Disable stories in docs page by default, then use one representative examples
+            disable: true,
+            // Use iframe to keep dropdown menu inside story for docs page
+            inlineStories: false,
+            iframeHeight: '300px',
+        },
+    },
+    // Use 'onChange' as a default arg, otherwise components throw an error when interacted with
+    // (Maybe this could be handled in the component - as a required prop)
+    args: { className: 'select', onChange },
+    argTypes: {
+        valid: { ...sharedPropTypes.statusArgType },
+        warning: { ...sharedPropTypes.statusArgType },
+        error: { ...sharedPropTypes.statusArgType },
+        clearText: { ...requiredIfArgType },
+        noMatchText: { ...requiredIfArgType },
+    },
 }
 
-export const WithOptions = () => (
-    <MultiSelect className="select">
+const WithOptionsTemplate = args => (
+    <MultiSelect {...args}>
         <MultiSelectOption value="1" label="option one" />
         <MultiSelectOption value="2" label="option two" />
         <MultiSelectOption value="3" label="option three" />
     </MultiSelect>
 )
-WithOptions.storyName = 'With options'
 
-export const WithOptionsAndOnChange = () => (
-    <MultiSelect
-        className="select"
-        onChange={val => console.log(val)}
-        selected={['1']}
-    >
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+const EmptyTemplate = args => <MultiSelect {...args} />
+
+export const WithOptionsAndOnChange = WithOptionsTemplate.bind({})
 WithOptionsAndOnChange.storyName = 'With options and onChange'
 
-export const WithOnFocus = () => (
-    <MultiSelect className="select" onFocus={window.onFocus}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+export const WithOptionsAndSelection = WithOptionsTemplate.bind({})
+WithOptionsAndSelection.args = { selected: ['1'] }
+WithOptionsAndSelection.storyName = 'With options and a selection'
+
+export const WithOnFocus = WithOptionsTemplate.bind({})
+WithOnFocus.args = { onFocus }
 WithOnFocus.storyName = 'With onFocus'
 
-export const WithOnBlur = () => (
-    <MultiSelect className="select" onBlur={window.onBlur}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+export const WithOnBlur = WithOptionsTemplate.bind({})
+WithOnBlur.args = { onBlur }
 WithOnBlur.storyName = 'With onBlur'
 
-export const WithCustomOptionsAndOnChange = () => (
-    <MultiSelect className="select" onChange={window.onChange}>
+export const WithCustomOptionsAndOnChange = args => (
+    <MultiSelect {...args}>
         <CustomMultiSelectOption value="1" label="option one" />
         <CustomMultiSelectOption value="2" label="option two" />
         <CustomMultiSelectOption value="3" label="option three" />
@@ -69,8 +98,8 @@ export const WithCustomOptionsAndOnChange = () => (
 )
 WithCustomOptionsAndOnChange.storyName = 'With custom options and onChange'
 
-export const WithInvalidOptions = () => (
-    <MultiSelect className="select">
+export const WithInvalidOptions = args => (
+    <MultiSelect {...args}>
         <div>invalid one</div>
         <MultiSelectOption value="1" label="option one" />
         <div>invalid two</div>
@@ -82,10 +111,9 @@ export const WithInvalidOptions = () => (
         {false}
     </MultiSelect>
 )
-WithInvalidOptions.storyName = 'With invalid options'
 
-export const WithInvalidFilterableOptions = () => (
-    <MultiSelect filterable className="select" noMatchText="No match">
+export const WithInvalidFilterableOptions = args => (
+    <MultiSelect {...args}>
         <div>invalid one</div>
         <MultiSelectOption value="1" label="option one" />
         <div>invalid two</div>
@@ -94,49 +122,38 @@ export const WithInvalidFilterableOptions = () => (
         <MultiSelectOption value="3" label="option three" />
     </MultiSelect>
 )
-WithInvalidFilterableOptions.storyName = 'With invalid filterable options'
+WithInvalidFilterableOptions.args = {
+    filterable: true,
+    noMatchText: 'No match found',
+}
 
-export const WithInitialFocus = () => (
-    <MultiSelect className="select" initialFocus />
-)
+export const WithInitialFocus = EmptyTemplate.bind({})
+WithInitialFocus.args = { initialFocus: true }
 WithInitialFocus.storyName = 'With initialFocus'
 
-export const Empty = () => <MultiSelect className="select" />
+export const Empty = EmptyTemplate.bind({})
 
-export const EmptyWithEmptyText = () => (
-    <MultiSelect className="select" empty="Custom empty text" />
-)
-EmptyWithEmptyText.storyName = 'Empty with empty text'
+export const EmptyWithEmptyText = EmptyTemplate.bind({})
+EmptyWithEmptyText.args = { empty: 'Custom empty text' }
 
-export const EmptyWithEmptyComponent = () => (
-    <MultiSelect
-        className="select"
-        empty={<div className="custom-empty">Custom empty component</div>}
-    />
-)
-EmptyWithEmptyComponent.storyName = 'Empty with empty component'
+export const EmptyWithEmptyComponent = EmptyTemplate.bind({})
+EmptyWithEmptyComponent.args = {
+    empty: <div className="custom-empty">Custom empty component</div>,
+}
 
-export const WithOptionsAndLoading = () => (
-    <MultiSelect className="select" loading>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithOptionsAndLoading.storyName = 'With options and loading'
+export const WithOptionsAndLoading = WithOptionsTemplate.bind({})
+WithOptionsAndLoading.args = { loading: true }
 
-export const WithOptionsLoadingAndLoadingText = () => (
-    <MultiSelect className="select" loadingText="Loading options" loading>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+export const WithOptionsLoadingAndLoadingText = WithOptionsTemplate.bind({})
+WithOptionsLoadingAndLoadingText.args = {
+    ...WithOptionsAndLoading.args,
+    loadingText: 'Loading options',
+}
 WithOptionsLoadingAndLoadingText.storyName =
     'With options, loading and loading text'
 
-export const WithMoreThanTenOptions = () => (
-    <MultiSelect className="select">
+export const WithMoreThanTenOptions = args => (
+    <MultiSelect {...args}>
         <MultiSelectOption value="1" label="option one" />
         <MultiSelectOption value="2" label="option two" />
         <MultiSelectOption value="3" label="option three" />
@@ -151,10 +168,9 @@ export const WithMoreThanTenOptions = () => (
         <MultiSelectOption value="12" label="option twelve" />
     </MultiSelect>
 )
-WithMoreThanTenOptions.storyName = 'With more than ten options'
 
-export const WithMoreThanThreeOptionsAndA100PxMaxHeight = () => (
-    <MultiSelect className="select" maxHeight="100px">
+export const WithMoreThanThreeOptionsAndA100PxMaxHeight = args => (
+    <MultiSelect {...args}>
         <MultiSelectOption value="1" label="option one" />
         <MultiSelectOption value="2" label="option two" />
         <MultiSelectOption value="3" label="option three" />
@@ -169,70 +185,42 @@ export const WithMoreThanThreeOptionsAndA100PxMaxHeight = () => (
         <MultiSelectOption value="12" label="option twelve" />
     </MultiSelect>
 )
+WithMoreThanThreeOptionsAndA100PxMaxHeight.args = { maxHeight: '100px' }
 WithMoreThanThreeOptionsAndA100PxMaxHeight.storyName =
     'With more than three options and a 100px max-height'
 
-export const WithOptionsASelectionAndDisabled = () => (
-    <MultiSelect disabled className="select" selected={['1']}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+export const WithOptionsAndDisabled = WithOptionsTemplate.bind({})
+WithOptionsAndDisabled.args = { disabled: true }
+WithOptionsAndDisabled.storyName = 'With options and disabled'
+
+export const WithOptionsASelectionAndDisabled = WithOptionsTemplate.bind({})
+WithOptionsASelectionAndDisabled.args = {
+    ...WithOptionsAndDisabled.args,
+    ...WithOptionsAndSelection.args,
+}
 WithOptionsASelectionAndDisabled.storyName =
     'With options, a selection and disabled'
 
-export const WithOptionsAndDisabled = () => (
-    <MultiSelect disabled className="select">
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithOptionsAndDisabled.storyName = 'With options and disabled'
+export const WithPrefix = WithOptionsTemplate.bind({})
+WithPrefix.args = { prefix: 'Prefix text' }
 
-export const WithPrefix = () => (
-    <MultiSelect className="select" prefix="Prefix text">
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithPrefix.storyName = 'With prefix'
+export const WithPrefixAndSelection = WithOptionsTemplate.bind({})
+WithPrefixAndSelection.args = {
+    ...WithPrefix.args,
+    ...WithOptionsAndSelection.args,
+}
 
-export const WithPrefixAndSelection = () => (
-    <MultiSelect className="select" prefix="Prefix text" selected={['1']}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithPrefixAndSelection.storyName = 'With prefix and selection'
+export const WithPlaceholder = WithOptionsTemplate.bind({})
+WithPlaceholder.args = { placeholder: 'Placeholder text' }
 
-export const WithPlaceholder = () => (
-    <MultiSelect className="select" placeholder="Placeholder text">
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithPlaceholder.storyName = 'With placeholder'
+export const WithPlaceholderAndSelection = WithOptionsTemplate.bind({})
+WithPlaceholderAndSelection.args = {
+    ...WithPlaceholder.args,
+    ...WithOptionsAndSelection.args,
+}
 
-export const WithPlaceholderAndSelection = () => (
-    <MultiSelect
-        className="select"
-        selected={['1']}
-        placeholder="Placeholder text"
-    >
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithPlaceholderAndSelection.storyName = 'With placeholder and selection'
-
-export const WithDisabledOptionAndOnChange = () => (
-    <MultiSelect className="select" onChange={window.onChange}>
+export const WithDisabledOptionAndOnChange = args => (
+    <MultiSelect {...args}>
         <MultiSelectOption value="1" label="option one" />
         <MultiSelectOption value="2" label="option two" />
         <MultiSelectOption value="3" label="option three" />
@@ -241,63 +229,24 @@ export const WithDisabledOptionAndOnChange = () => (
 )
 WithDisabledOptionAndOnChange.storyName = 'With disabled option and onChange'
 
-export const WithOptionsAndASelection = () => (
-    <MultiSelect className="select" selected={['1']}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithOptionsAndASelection.storyName = 'With options and a selection'
+export const WithOptionsAndMultipleSelections = WithOptionsTemplate.bind({})
+WithOptionsAndMultipleSelections.args = { selected: ['1', '2'] }
 
-export const WithOptionsASelectionAndOnChange = () => (
-    <MultiSelect className="select" selected={['1']} onChange={window.onChange}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithOptionsASelectionAndOnChange.storyName =
-    'With options, a selection and onChange'
-
-export const WithOptionsAndMultipleSelections = () => (
-    <MultiSelect className="select" selected={['1', '2']}>
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithOptionsAndMultipleSelections.storyName =
-    'With options and multiple selections'
-
-export const WithClearButtonSelectionAndOnChange = () => (
-    <MultiSelect
-        clearable
-        clearText="Clear"
-        className="select"
-        selected={['1']}
-        onChange={window.onChange}
-    >
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
+export const WithClearButtonSelectionAndOnChange = WithOptionsTemplate.bind({})
+WithClearButtonSelectionAndOnChange.args = {
+    ...WithOptionsAndSelection.args,
+    clearable: true,
+    clearText: 'Clear',
+}
 WithClearButtonSelectionAndOnChange.storyName =
     'With clear button, selection and onChange'
 
-export const WithFilterField = () => (
-    <MultiSelect filterable noMatchText="No match found" className="select">
-        <MultiSelectOption value="1" label="option one" />
-        <MultiSelectOption value="2" label="option two" />
-        <MultiSelectOption value="3" label="option three" />
-    </MultiSelect>
-)
-WithFilterField.storyName = 'With filter field'
+export const WithFilterField = WithOptionsTemplate.bind({})
+WithFilterField.args = { ...WithInvalidFilterableOptions.args }
 
-export const DefaultPosition = () => (
+export const DefaultPosition = args => (
     <>
-        <MultiSelect className="select">
+        <MultiSelect {...args}>
             <MultiSelectOption value="1" label="option one" />
             <MultiSelectOption value="2" label="option two" />
             <MultiSelectOption value="3" label="option three" />
@@ -318,11 +267,10 @@ export const DefaultPosition = () => (
         `}</style>
     </>
 )
-DefaultPosition.storyName = 'Default position'
 
-export const FlippedPosition = () => (
+export const FlippedPosition = args => (
     <>
-        <MultiSelect className="select">
+        <MultiSelect {...args}>
             <MultiSelectOption value="1" label="option one" />
             <MultiSelectOption value="2" label="option two" />
             <MultiSelectOption value="3" label="option three" />
@@ -350,11 +298,10 @@ export const FlippedPosition = () => (
         `}</style>
     </>
 )
-FlippedPosition.storyName = 'Flipped position'
 
-export const ShiftedIntoView = () => (
+export const ShiftedIntoView = args => (
     <>
-        <MultiSelect className="select">
+        <MultiSelect {...args}>
             <MultiSelectOption value="1" label="option one" />
             <MultiSelectOption value="2" label="option two" />
             <MultiSelectOption value="3" label="option three" />
@@ -382,4 +329,3 @@ export const ShiftedIntoView = () => (
         `}</style>
     </>
 )
-ShiftedIntoView.storyName = 'Shifted into view'
