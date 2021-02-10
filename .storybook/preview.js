021addaeb49ec7ddcc8cfee02e20452e1c1d9f91
Hendrik de Graaf <hendrik@dhis2.org>
2021-02-10 09:44:49 +0100

    fix(storybook): tweak jsx settings to omit undefined and default props

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -41,7 +41,11 @@ addParameters({
     options: {
         storySort: (a, b) =>
             a[1].kind === b[1].kind
-            ? 0
-            : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+                ? 0
+                : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    },
+    jsx: {
+        filterProps: val => val !== undefined,
+        showDefaultProps: false,
     },
 })
