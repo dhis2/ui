b46ee78d89e13c89246b34d1c0dfed1a50372176
Kai Vandivier <kpvandivier@gmail.com>
2021-02-12 17:16:15 +0100

    refactor: react prop-types and prop annotations

--- packages/widgets/src/FieldGroup/FieldGroup.js
+++ packages/widgets/src/FieldGroup/FieldGroup.js
@@ -1,6 +1,6 @@
-import propTypes from '@dhis2/prop-types'
 import { sharedPropTypes } from '@dhis2/ui-constants'
 import { Field, FieldSet } from '@dhis2/ui-core'
+import PropTypes from 'prop-types'
 import React from 'react'
 
 /**
@@ -66,17 +66,26 @@ FieldGroup.defaultProps = {
  * @prop {boolean} [error]
  */
 FieldGroup.propTypes = {
-    children: propTypes.node,
-    className: propTypes.string,
-    dataTest: propTypes.string,
-    disabled: propTypes.bool,
+    children: PropTypes.node,
+    className: PropTypes.string,
+    dataTest: PropTypes.string,
+    /** Disables the form controls within */
+    disabled: PropTypes.bool,
+    /** Applies 'error' styling to validation text for feedback. Mutually exclusive with `warning` and `valid` props */
     error: sharedPropTypes.statusPropType,
-    helpText: propTypes.string,
-    label: propTypes.string,
-    name: propTypes.string,
-    required: propTypes.bool,
+    /** Useful instructions for the user */
+    helpText: PropTypes.string,
+    /** Labels the Field Group */
+    label: PropTypes.string,
+    /** Name associate with the Field Group. Passed in object as argument to event handlers */
+    name: PropTypes.string,
+    /** Adds an asterisk to indicate this field is required */
+    required: PropTypes.bool,
+    /** Applies 'valid' styling to validation text for feedback. Mutually exclusive with `warning` and `error` props */
     valid: sharedPropTypes.statusPropType,
-    validationText: propTypes.string,
+    /** Adds text at the bottom of the field to provide validation feedback. Acquires styles from `valid`, `warning` and `error` statuses */
+    validationText: PropTypes.string,
+    /** Applies 'warning' styling to validation text for feedback. Mutually exclusive with `valid` and `error` props */
     warning: sharedPropTypes.statusPropType,
 }
 
