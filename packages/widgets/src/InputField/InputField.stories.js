6012d47d39790c61f80fd024edb709aeed78b94c
Kai Vandivier <kpvandivier@gmail.com>
2021-02-15 13:13:07 +0100

    docs(input-field): renovate stories

--- packages/widgets/src/InputField/InputField.stories.js
+++ packages/widgets/src/InputField/InputField.stories.js
@@ -1,127 +1,161 @@
+import { sharedPropTypes } from '@dhis2/ui-constants'
 import React from 'react'
 import { InputField } from './InputField.js'
 
-const logger = ({ name, value }) => console.info(`${name}: ${value}`)
+const subtitle = 'Allows a user to enter data, usually text'
 
-const props = {
-    label: 'Default label',
-    name: 'Default',
-    onChange: logger,
+const description = `
+Inputs are used wherever a user needs to input standard text information. Inputs are often used as part of forms. An input can also be used to capture information outside of a form, perhaps as a 'Filter' or 'Search' field.
+
+InputField wraps an Input component with a label, help text, validation text, and some other features.
+
+Please see more about options and features of inputs at [Design System: Input Field](https://github.com/dhis2/design-system/blob/master/atoms/inputfield.md#input).
+
+\`\`\`js
+import { InputField } from '@dhis2/ui'
+\`\`\`
+`
+
+const logger = ({ name, value }) =>
+    console.log(`Name: ${name}, value: ${value}`)
+
+const inputTypeArgType = {
+    table: { type: { summary: 'string' } },
+    control: {
+        type: 'select',
+        options: [
+            'text',
+            'number',
+            'password',
+            'email',
+            'url',
+            'tel',
+            'date',
+            'datetime',
+            'datetime-local',
+            'month',
+            'week',
+            'time',
+            'search',
+        ],
+    },
 }
 
 export default {
     title: 'Forms/Input/Input Field',
     component: InputField,
+    parameters: {
+        componentSubtitle: subtitle,
+        docs: { description: { component: description } },
+    },
+    // Default args
+    args: {
+        label: 'Default label',
+        name: 'defaultName',
+        onChange: logger,
+    },
+    argTypes: {
+        type: { ...inputTypeArgType },
+        valid: { ...sharedPropTypes.statusArgType },
+        warning: { ...sharedPropTypes.statusArgType },
+        error: { ...sharedPropTypes.statusArgType },
+    },
 }
 
-export const Default = () => <InputField name="nolabel" onChange={logger} />
+const Template = args => <InputField {...args} />
 
-export const NoPlaceholderNoValue = () => <InputField {...props} />
+export const Default = Template.bind({})
+
+export const NoPlaceholderNoValue = Template.bind({})
 NoPlaceholderNoValue.storyName = 'No placeholder, no value'
 
-export const PlaceholderNoValue = () => (
-    <InputField {...props} placeholder="Hold the place" />
-)
+export const PlaceholderNoValue = Template.bind({})
+PlaceholderNoValue.args = { placeholder: 'Hold the place' }
 PlaceholderNoValue.storyName = 'Placeholder, no value'
 
-export const WithHelpText = () => (
-    <InputField
-        {...props}
-        placeholder="Hold the place"
-        helpText="With some helping text to guide the user along"
-    />
-)
-WithHelpText.storyName = 'With Help text'
+export const WithHelpText = Template.bind({})
+WithHelpText.args = {
+    ...PlaceholderNoValue.args,
+    helpText: 'With some helping text to guide the user along',
+}
 
-export const WithValue = () => (
-    <InputField
-        {...props}
-        value="This is set through the value prop, which means the component is controlled."
-    />
-)
-WithValue.storyName = 'With value'
+export const WithValue = Template.bind({})
+WithValue.args = {
+    value:
+        'This is set through the value prop, which means the component is controlled.',
+}
 
-export const Focus = () => <InputField {...props} initialFocus />
+export const Focus = Template.bind({})
+Focus.args = { initialFocus: true }
+// Disabled initial focus stories on docs page
+Focus.parameters = { docs: { disable: true } }
 
-export const StatusValid = () => (
-    <InputField {...props} value="This value is valid" valid />
-)
+export const StatusValid = Template.bind({})
+StatusValid.args = { valid: true, value: 'This value is valid' }
 StatusValid.storyName = 'Status: Valid'
 
-export const StatusWarning = () => (
-    <InputField {...props} value="This value produces a warning" warning />
-)
+export const StatusWarning = Template.bind({})
+StatusWarning.args = { warning: true, value: 'This value produces a warning' }
 StatusWarning.storyName = 'Status: Warning'
 
-export const StatusError = () => (
-    <InputField
-        {...props}
-        error
-        value="This value produces an error"
-        helpText="This is some help text to advice what this input actually is."
-        validationText="This describes the error, if a message is supplied."
-    />
-)
+export const StatusError = Template.bind({})
+StatusError.args = {
+    error: true,
+    value: 'This value produces an error',
+    helpText: 'This is some help text to advise what this input actually is.',
+    validationText:
+        'This validation text describes the error, if a message is supplied.',
+}
 StatusError.storyName = 'Status: Error'
 
-export const StatusLoading = () => (
-    <InputField
-        {...props}
-        value="This value produces a loading state"
-        loading
-    />
-)
+export const StatusLoading = Template.bind({})
+StatusLoading.args = {
+    loading: true,
+    value: 'This value produces a loading state',
+}
 StatusLoading.storyName = 'Status: Loading'
 
-export const Disabled = () => (
-    <InputField {...props} value="This field is disabled" disabled />
-)
+export const Disabled = Template.bind({})
+Disabled.args = { disabled: true, value: 'This field is disabled' }
 
-export const ReadOnly = () => (
-    <InputField {...props} value="This field is disabled" readOnly />
-)
-ReadOnly.storyName = 'Read only'
+export const ReadOnly = Template.bind({})
+ReadOnly.args = { readOnly: true, value: 'This field is read-only' }
 
-export const Dense = () => (
-    <InputField {...props} value="This field is dense" dense />
-)
+export const Dense = Template.bind({})
+Dense.args = { dense: true, value: 'This field is dense' }
 
-export const InputWidth = () => (
+export const InputWidth = args => (
     <>
         <InputField
+            {...args}
             name="input1"
             label="My textarea has a width of 100px"
             inputWidth="100px"
-            onChange={logger}
         />
         <InputField
+            {...args}
             name="input2"
             label="My textarea has a width of 220px"
             inputWidth="220px"
-            onChange={logger}
         />
     </>
 )
-InputWidth.storyName = 'Input width'
 
-export const LabelTextOverflow = () => (
-    <InputField
-        {...props}
-        label="This label is too long to show on a single line of the input field's label. We just let it flow to the next line so the user can still read it. However, we should always aim to keep it shorter than this!"
-        dense
-        warning
-    />
-)
-LabelTextOverflow.storyName = 'Label text overflow'
+export const LabelTextOverflow = Template.bind({})
+LabelTextOverflow.args = {
+    dense: true,
+    warning: true,
+    label:
+        "This label is too long to show on a single line of the input field's label. We just let it flow to the next line so the user can still read it. However, we should always aim to keep it shorter than this!",
+}
 
-export const ValueTextOverflow = () => (
-    <InputField
-        {...props}
-        value="This value is too long in order to show on a single line of the input field. It should stay on one line, not in an extra line and which wouldn't look like a standard input"
-        dense
-        warning
-    />
-)
-ValueTextOverflow.storyName = 'Value text overflow'
+export const ValueTextOverflow = Template.bind({})
+ValueTextOverflow.args = {
+    value:
+        "This value is too long in order to show on a single line of the input field. It should stay on one line, not in an extra line and which wouldn't look like a standard input",
+    dense: true,
+    warning: true,
+}
 
-export const Required = () => <InputField {...props} required />
+export const Required = Template.bind({})
+Required.args = { required: true }
