61a502d3c3b6e6f2a16c16756d320c7bc2b3a8cd
Kai Vandivier <kpvandivier@gmail.com>
2021-02-16 14:48:46 +0100

    docs(multiselect-field-ff): add description

--- packages/forms/src/MultiSelectFieldFF/MultiSelectFieldFF.stories.js
+++ packages/forms/src/MultiSelectFieldFF/MultiSelectFieldFF.stories.js
@@ -3,6 +3,30 @@ import { Field } from 'react-final-form'
 import { formDecorator } from '../formDecorator.js'
 import { MultiSelectFieldFF } from './MultiSelectFieldFF.js'
 
+const description = `
+The \`MultiSelectFieldFF\` is a wrapper around a \`MultiSelectField\` that enables it to work with Final Form, the preferred library for form validation and utilities in DHIS 2 apps.
+
+#### Final Form
+
+See how to use Final Form at [Final Form - Getting Started](https://final-form.org/docs/react-final-form/getting-started).
+
+Inside a Final Form \`<Form>\` component, these 'FF' UI components are intended to be used in the \`component\` prop of the [Final Form \`<Field>\` components](https://final-form.org/docs/react-final-form/api/Field) where they will receive some props from the Field, e.g. \`<Field component={MultiSelectFieldFF} />\`.  See the code samples below for examples.
+
+#### Props
+
+The props shown in the table below are generally provided to the \`MultiSelectFieldFF\` wrapper by the Final Form \`Field\`.
+
+Note that any props beyond the API of the \`Field\` component will be spread to the \`MultiSelectFieldFF\`, which passes any extra props to the underlying \`MultiSelectField\` using \`{...rest}\`.
+
+Therefore, to add any props to the \`MultiSelectFieldFF\` or \`MultiSelectField\`, add those props to the parent Final Form \`Field\` component.
+
+Also see \`MultiSelect\` and \`MultiSelectField\` for notes about props and implementation.
+
+\`\`\`js
+import { MultiSelectFieldFF } from '@dhis2/ui'
+\`\`\`
+`
+
 const options = [
     { value: '1', label: 'one' },
     { value: '2', label: 'two' },
@@ -22,6 +46,7 @@ export default {
     title: 'Forms/Multi Select/Multi Select Field (Final Form)',
     component: MultiSelectFieldFF,
     decorators: [formDecorator],
+    parameters: { docs: { description: { component: description } } },
 }
 
 export const Default = () => (
