777c63cf87fdc3357b96b97d1e1929d0a9e0b0b9
Kai Vandivier <kpvandivier@gmail.com>
2021-02-16 14:42:00 +0100

    docs(input-field-ff): add description

--- packages/forms/src/InputFieldFF/InputFieldFF.stories.js
+++ packages/forms/src/InputFieldFF/InputFieldFF.stories.js
@@ -4,10 +4,35 @@ import { formDecorator } from '../formDecorator.js'
 import { hasValue } from '../validators/index.js'
 import { InputFieldFF } from './InputFieldFF.js'
 
+const description = `
+The \`InputFieldFF\` is a wrapper around a \`InputField\` that enables it to work with Final Form, the preferred library for form validation and utilities in DHIS 2 apps.
+
+#### Final Form
+
+See how to use Final Form at [Final Form - Getting Started](https://final-form.org/docs/react-final-form/getting-started).
+
+Inside a Final Form \`<Form>\` component, these 'FF' UI components are intended to be used in the \`component\` prop of the [Final Form \`<Field>\` components](https://final-form.org/docs/react-final-form/api/Field) where they will receive some props from the Field, e.g. \`<Field component={InputFieldFF} />\`.  See the code samples below for examples.
+
+#### Props
+
+The props shown in the table below are generally provided to the \`InputFieldFF\` wrapper by the Final Form \`Field\`.
+
+Note that any props beyond the API of the \`Field\` component will be spread to the \`InputFieldFF\`, which passes any extra props to the underlying \`InputField\` using \`{...rest}\`.
+
+Therefore, to add any props to the \`InputFieldFF\` or \`InputField\`, add those props to the parent Final Form \`Field\` component.
+
+Also see \`InputField\` for notes about props and implementation.
+
+\`\`\`js
+import { InputFieldFF } from '@dhis2/ui'
+\`\`\`
+`
+
 export default {
     title: 'Forms/Input/Input Field (Final Form)',
     component: InputFieldFF,
     decorators: [formDecorator],
+    parameters: { docs: { description: { component: description } } },
 }
 
 export const Default = () => (
