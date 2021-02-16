67c9619978b3a3f3e95556fb2d90a6a6858efc48
Kai Vandivier <kpvandivier@gmail.com>
2021-02-16 16:05:46 +0100

    docs(radio-field-ff): add description

--- packages/forms/src/RadioFieldFF/RadioFieldFF.stories.js
+++ packages/forms/src/RadioFieldFF/RadioFieldFF.stories.js
@@ -4,10 +4,35 @@ import { Field } from 'react-final-form'
 import { formDecorator } from '../formDecorator.js'
 import { RadioFieldFF } from './RadioFieldFF.js'
 
+const description = `
+The \`RadioFieldFF\` is a wrapper around a \`Radio\` that enables it to work with Final Form, the preferred library for form validation and utilities in DHIS 2 apps.
+
+#### Final Form
+
+See how to use Final Form at [Final Form - Getting Started](https://final-form.org/docs/react-final-form/getting-started).
+
+Inside a Final Form \`<Form>\` component, these 'FF' UI components are intended to be used in the \`component\` prop of the [Final Form \`<Field>\` components](https://final-form.org/docs/react-final-form/api/Field) where they will receive some props from the Field, e.g. \`<Field component={RadioFieldFF} />\`.  See the code samples below for examples.
+
+#### Props
+
+The props shown in the table below are generally provided to the \`RadioFieldFF\` wrapper by the Final Form \`Field\`.
+
+Note that any props beyond the API of the \`Field\` component will be spread to the \`RadioFieldFF\`, which passes any extra props to the underlying \`Radio\` using \`{...rest}\`.
+
+Therefore, to add any props to the \`RadioFieldFF\` or \`Radio\`, add those props to the parent Final Form \`Field\` component.
+
+Also see \`Radio\` for notes about props and implementation.
+
+\`\`\`js
+import { RadioFieldFF } from '@dhis2/ui'
+\`\`\`
+`
+
 export default {
     title: 'Forms/Radio/Radio Field (Final Form)',
     component: RadioFieldFF,
     decorators: [formDecorator],
+    parameters: { docs: { description: { component: description } } },
 }
 
 export const Default = () => (
