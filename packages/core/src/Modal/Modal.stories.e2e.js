69118de95c5667331460b1ecaf968d13d6b8fc9d
Kai Vandivier <kpvandivier@gmail.com>
2021-02-25 14:00:20 +0100

    test(modal): should close on click above or below modal

--- packages/core/src/Modal/Modal.stories.e2e.js
+++ packages/core/src/Modal/Modal.stories.e2e.js
@@ -24,4 +24,16 @@ storiesOf('Modal', module)
             </ModalActions>
         </Modal>
     ))
+    .add('Bottom-aligned, with onClose', () => (
+        <Modal small onClose={window.onClose} position="bottom">
+            <ModalTitle>Title</ModalTitle>
+            <ModalContent>Content</ModalContent>
+            <ModalActions>
+                <ButtonStrip end>
+                    <Button secondary>Secondary action</Button>
+                    <Button primary>Primary action</Button>
+                </ButtonStrip>
+            </ModalActions>
+        </Modal>
+    ))
     .add('With children', () => <Modal>I am a child</Modal>)
