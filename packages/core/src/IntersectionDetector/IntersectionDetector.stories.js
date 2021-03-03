5d3f4cfcb646f564def101234395a30579cbc57f
Kai Vandivier <kpvandivier@gmail.com>
2021-03-04 00:24:52 +0100

    docs(intersection-detector): fix broken stories; add descriptions

--- packages/core/src/IntersectionDetector/IntersectionDetector.stories.js
+++ packages/core/src/IntersectionDetector/IntersectionDetector.stories.js
@@ -1,26 +1,45 @@
-/* eslint-disable prettier/prettier, react/prop-types, react/display-name */
+import PropTypes from 'prop-types'
 import React, { useState, useRef } from 'react'
 import { IntersectionDetector } from './IntersectionDetector.js'
 
+const description = `
+A tool to performantly detect when two components intersect, which is often better to use than scroll listeners. A common use case is to detect when a child component is in view inside a scrolling parent. Uses an [\`IntersectionObserver\`](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API) object internally.
+
+See the code examples below for examples on how to implement it.
+
+\`\`\`js
+import { IntersectionDetector } from '@dhis2/ui'
+\`\`\`
+`
+
+export default {
+    title: 'Helpers/Intersection Observer',
+    component: IntersectionDetector,
+    parameters: {
+        docs: {
+            description: { component: description },
+            source: { type: 'code' },
+        },
+    },
+    // Hide 'container styles' from Template in args table
+    argTypes: { containerStyles: { table: { disable: true } } },
+}
+
 const Text = () => (
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Cras tempor venenatis hendrerit. Donec dictum sed ligula id
-        efficitur. Suspendisse feugiat, elit in dictum imperdiet,
-        mi tellus euismod nibh, vitae hendrerit turpis odio ut
-        mauris. Vestibulum rhoncus interdum nunc eu eleifend.
-        Aenean viverra nibh hendrerit nulla iaculis, vitae
-        tincidunt erat ullamcorper. Donec tempus mattis faucibus.
-        Donec nec lacus vitae elit aliquet pharetra. Cras vitae
-        odio eu lorem euismod malesuada.  Nunc eu rhoncus mauris.
-        Nullam vehicula elit id vehicula maximus.  Phasellus
-        gravida tincidunt mauris, vitae laoreet erat commodo id.
-        Nullam vitae erat ante. Proin id ultricies risus, in
-        ultricies mauris.  Vivamus lectus enim, ultricies vel
-        egestas nec, tempor a magna. Nam sed fermentum ipsum, a
-        ullamcorper felis. Aenean finibus erat elit, at eleifend
-        nulla rutrum at.
-
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor
+        venenatis hendrerit. Donec dictum sed ligula id efficitur. Suspendisse
+        feugiat, elit in dictum imperdiet, mi tellus euismod nibh, vitae
+        hendrerit turpis odio ut mauris. Vestibulum rhoncus interdum nunc eu
+        eleifend. Aenean viverra nibh hendrerit nulla iaculis, vitae tincidunt
+        erat ullamcorper. Donec tempus mattis faucibus. Donec nec lacus vitae
+        elit aliquet pharetra. Cras vitae odio eu lorem euismod malesuada. Nunc
+        eu rhoncus mauris. Nullam vehicula elit id vehicula maximus. Phasellus
+        gravida tincidunt mauris, vitae laoreet erat commodo id. Nullam vitae
+        erat ante. Proin id ultricies risus, in ultricies mauris. Vivamus lectus
+        enim, ultricies vel egestas nec, tempor a magna. Nam sed fermentum
+        ipsum, a ullamcorper felis. Aenean finibus erat elit, at eleifend nulla
+        rutrum at.
         <style jsx>{`
             p {
                 margin: 0;
@@ -29,112 +48,76 @@ const Text = () => (
     </p>
 )
 
-const SizeLimitDecorator = ({ rootRef, isIntersecting, children }) => (
-    <div className="container">
-        <p>Is intersecting: {` ${isIntersecting}`}</p>
-
-        <div className="scrollContainer" ref={rootRef}>
-            <div className="contentContainer">
-                {children}
-            </div>
-        </div>
-
-        <style jsx>{`
-            .scrollContainer {
-                //border: 1px solid black;
-                width: 200px;
-                height: 300px;
-                overflow: hidden;
-                overflow-y: auto;
-            }
-
-            .contentContainer {
-                position: relative;
-            }
-        `}</style>
-    </div>
-)
-
-const sizeLimitDecorator = tolerance => fn => {
+const Template = ({ containerStyles, ...args }) => {
     const rootRef = useRef()
     const [isIntersecting, setIsIntersecting] = useState(false)
 
     return (
-        <SizeLimitDecorator tolerance={tolerance} rootRef={rootRef} isIntersecting={isIntersecting}>
-            {fn({ setIsIntersecting, rootRef })}
-        </SizeLimitDecorator>
+        <div className="container">
+            <p>Is intersecting: {` ${isIntersecting}`}</p>
+
+            <div className="scrollContainer" ref={rootRef}>
+                <div className="contentContainer">
+                    <Text />
+
+                    <div style={containerStyles}>
+                        <IntersectionDetector
+                            {...args}
+                            onChange={({ isIntersecting }) =>
+                                setIsIntersecting(isIntersecting)
+                            }
+                            rootRef={rootRef}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <style jsx>{`
+                .scrollContainer {
+                    width: 200px;
+                    height: 300px;
+                    overflow: hidden;
+                    overflow-y: auto;
+                }
+
+                .contentContainer {
+                    position: relative;
+                }
+            `}</style>
+        </div>
     )
 }
+Template.propTypes = { containerStyles: PropTypes.shape({}) }
 
-export default {
-    title: 'Helpers/Intersection Observer',
-    decorators: [sizeLimitDecorator(100)]
+const boxStyles = {
+    boxSizing: 'border-box',
+    border: '1px solid #f76a8c',
+    background: 'rgba(246,172,200,0.4)',
+    width: '100%',
+    height: '100px',
 }
 
-export const AbsoluteBottomArea = ({ setIsIntersecting, rootRef }) => (
-    <>
-        <Text />
+export const AbsoluteBottomArea = Template.bind({})
+AbsoluteBottomArea.args = {
+    containerStyles: {
+        ...boxStyles,
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
+        zIndex: '-1',
+    },
+}
 
-        <div style={{
-            boxSizing: 'border-box',
-            border: '1px solid #f76a8c',
-            background: 'rgba(246,172,200,0.4)',
-            width: '100%',
-            height: '100px',
-            position: 'absolute',
-            bottom: '0',
-            left: '0',
-            zIndex: '-1',
-        }}>
-            <IntersectionDetector
-                onChange={({ isIntersecting }) => setIsIntersecting(isIntersecting)}
-                rootRef={rootRef}
-            />
-        </div>
-    </>
-)
+export const AbsoluteTopArea = Template.bind({})
+AbsoluteTopArea.args = {
+    containerStyles: {
+        ...boxStyles,
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        zIndex: '-1',
+    },
+}
 
-export const AbsoluteTopArea = ({ setIsIntersecting, rootRef }) => (
-    <>
-        <Text />
-
-        <div style={{
-            boxSizing: 'border-box',
-            border: '1px solid #f76a8c',
-            background: 'rgba(246,172,200,0.4)',
-            width: '100%',
-            height: '100px',
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            zIndex: '-1',
-        }}>
-            <IntersectionDetector
-                onChange={({ isIntersecting }) => setIsIntersecting(isIntersecting)}
-                rootRef={rootRef}
-            />
-        </div>
-    </>
-)
-
-export const BottomArea = ({ setIsIntersecting, rootRef }) => (
-    <>
-        <Text />
-
-        <div style={{
-            boxSizing: 'border-box',
-            border: '1px solid #f76a8c',
-            background: 'rgba(246,172,200,0.4)',
-        }}>
-            <IntersectionDetector
-                onChange={({ isIntersecting }) => setIsIntersecting(isIntersecting)}
-                rootRef={rootRef}
-            >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Cras
-                tempor venenatis hendrerit. Donec dictum sed ligula id
-                efficitur.
-            </IntersectionDetector>
-
-        </div>
-    </>
-)
+export const BottomArea = Template.bind({})
+BottomArea.args = { containerStyles: { ...boxStyles } }
