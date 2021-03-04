f9f15e9d83e820ec23b1a42667c45f21bc628101
KaiVandivier <49666798+KaiVandivier@users.noreply.github.com>
2021-03-04 14:21:09 +0100

    docs: add stories for Input [LIBS-138] (#530)
    * docs: add stories for Input component
    
    * refactor(input): use react prop-types; add prop annotations

--- packages/core/src/Input/Input.js
+++ packages/core/src/Input/Input.js
@@ -1,6 +1,6 @@
-import propTypes from '@dhis2/prop-types'
 import { theme, colors, spacers, sharedPropTypes } from '@dhis2/ui-constants'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import css from 'styled-jsx/css'
 import { StatusIcon } from '../Icons/index.js'
@@ -219,24 +219,33 @@ Input.defaultProps = {
  * @prop {string} [dataTest]
  */
 Input.propTypes = {
-    className: propTypes.string,
-    dataTest: propTypes.string,
-    dense: propTypes.bool,
-    disabled: propTypes.bool,
+    className: PropTypes.string,
+    dataTest: PropTypes.string,
+    /** Makes the input smaller */
+    dense: PropTypes.bool,
+    /** Disables the input */
+    disabled: PropTypes.bool,
+    /** Applies 'error' appearance for validation feedback. Mutually exclusive with `valid` and `warning` props */
     error: sharedPropTypes.statusPropType,
-    initialFocus: propTypes.bool,
-    loading: propTypes.bool,
+    /** The input grabs initial focus on the page */
+    initialFocus: PropTypes.bool,
+    /** Adds a loading indicator beside the input */
+    loading: PropTypes.bool,
     /** The [native `max` attribute](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefmax), for use when `type` is `'number'` */
-    max: propTypes.string,
+    max: PropTypes.string,
     /** The [native `min` attribute](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefmin), for use when `type` is `'number'` */
-    min: propTypes.string,
-    name: propTypes.string,
-    placeholder: propTypes.string,
-    readOnly: propTypes.bool,
+    min: PropTypes.string,
+    /** Name associated with the input. Passed to event handler callbacks in object */
+    name: PropTypes.string,
+    /** Placeholder text for the input */
+    placeholder: PropTypes.string,
+    /** Makes the input read-only */
+    readOnly: PropTypes.bool,
     /** The [native `step` attribute](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefstep), for use when `type` is `'number'` */
-    step: propTypes.string,
-    tabIndex: propTypes.string,
-    type: propTypes.oneOf([
+    step: PropTypes.string,
+    tabIndex: PropTypes.string,
+    /** The native input `type` attribute */
+    type: PropTypes.oneOf([
         'text',
         'number',
         'password',
@@ -251,10 +260,16 @@ Input.propTypes = {
         'time',
         'search',
     ]),
+    /** Applies 'valid' appearance for validation feedback. Mutually exclusive with `error` and `warning` props */
     valid: sharedPropTypes.statusPropType,
-    value: propTypes.string,
+    /** Value in the input. Can be used to control the component (recommended). Passed to event handler callbacks in object */
+    value: PropTypes.string,
+    /** Applies 'warning' appearance for validation feedback. Mutually exclusive with `valid` and `error` props */
     warning: sharedPropTypes.statusPropType,
-    onBlur: propTypes.func,
-    onChange: propTypes.func,
-    onFocus: propTypes.func,
+    /** Called with signature `({ name: string, value: string }, event)` */
+    onBlur: PropTypes.func,
+    /** Called with signature `({ name: string, value: string }, event)` */
+    onChange: PropTypes.func,
+    /** Called with signature `({ name: string, value: string }, event)` */
+    onFocus: PropTypes.func,
 }
