96e1134639fa57bbf5f3d1152e0724b0d5626149
Médi-Rémi Hashim <4295266+mediremi@users.noreply.github.com>
2021-03-11 12:16:41 +0000

    fix: update LinearLoader story text (#541)

--- packages/core/src/LinearLoader/LinearLoader.stories.js
+++ packages/core/src/LinearLoader/LinearLoader.stories.js
@@ -8,10 +8,10 @@ const subtitle = `Used to inform the user that an element or page is in a loadin
 const description = `
 Use loading indicators whenever a component or application takes longer than 700ms to load. After this time a loader should be displayed so that the user can understand what is happening: loading is in progress. Consider that without a loading indicator a user would be unsure of their current status, so they are important UI elements.
 
-A circular loader is used when the loading time is uncertain and cannot be displayed as a percentage. A circular loader can spin many times, and each spin does not represent any amount of completion.
+A linear loader is used when the loading time is can be measured and displayed as a percentage.
 
 \`\`\`js
-import { CircularLoader } from '@dhis2/ui'
+import { LinearLoader } from '@dhis2/ui'
 \`\`\`
 `
 
