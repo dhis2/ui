9a52743000959b016ced28c11256ade521a1e133
Kai Vandivier <kpvandivier@gmail.com>
2021-03-26 00:02:49 +0100

    docs: transform relative links to github links; improve script

--- docs/fetch-design-system-pages.js
+++ docs/fetch-design-system-pages.js
@@ -4,10 +4,28 @@ const path = require('path')
 const { startCase } = require('lodash')
 const fetch = require('node-fetch')
 
-// Maybe I also want to load those images from the design system
-// Maybe I can just transform all the relative links to github links?
-// base url: https://raw.githubusercontent.com/dhis2/design-system/master/  ...images/something.png
+/**
+ * This regex matches relative links in markdown, like '[link](../images/image.png)'
+ * There is a negative lookahead assertion to exclude external links that include 'http'
+ * The capture groups for the above link would be 1. 'link' and 2. '../images/image.png'
+ * Check it out here: https://regex101.com/r/y5o9Tf/1
+ *
+ * This will be used to transform relative links (and images) in the design system
+ * pages into external links to the github repo so these pages don't need to serve
+ * images and to enable linking to other design system pages.
+ */
+const mdLocalLinkRegex = /\[(.*)\]\((?!http)(.*)\)/g
+const designSystemGithubUrl =
+    'https://raw.githubusercontent.com/dhis2/design-system/master/principles/'
 
+const replaceRelativeLinksWithGithubLinks = text =>
+    text.replaceAll(
+        mdLocalLinkRegex,
+        (_, text, linkPath) =>
+            `[${text}](${path.join(designSystemGithubUrl, linkPath)})`
+    )
+
+// Template for creating .stories.mdx file for storybook that imports from regular markdown file
 const mdxTemplate = basename => `import { Meta, Description } from '@storybook/addon-docs/blocks'
 import ${startCase(basename).replaceAll(' ', '')} from './${basename}.md'
 
@@ -18,26 +36,28 @@ import ${startCase(basename).replaceAll(' ', '')} from './${basename}.md'
 
 const outputDir = path.join('docs', 'design-system')
 
-const downloadAndProcessFiles = filteredFiles =>
-    Promise.all(
-        filteredFiles.map(({ name, download_url }) => {
+const downloadAndProcessFiles = async filteredFiles => {
+    // Create output dir if one doesn't exist yet
+    await fs.mkdir(outputDir, { recursive: true })
+    return Promise.all(
+        filteredFiles.map(async ({ name, download_url }) => {
+            // Get file contents
+            const text = await fetch(download_url).then(res => res.text())
+            // Transform relative links (see comments above)
+            const processedText = replaceRelativeLinksWithGithubLinks(text)
+            // Make a regular markdown file from contents
+            await fs.writeFile(path.join(outputDir, name), processedText)
+            // Make an MDX stories file that imports regular markdown
             const basename = path.basename(name, '.md')
-            fetch(download_url)
-                .then(res => res.text())
-                // Make a regular markdown file
-                .then(text => fs.writeFile(path.join(outputDir, name), text))
-                // Make an MDX stories file that imports regular markdown
-                .then(() =>
-                    fs.writeFile(
-                        path.join(outputDir, `${basename}.stories.mdx`),
-                        mdxTemplate(basename)
-                    )
-                )
+            await fs.writeFile(
+                path.join(outputDir, `${basename}.stories.mdx`),
+                mdxTemplate(basename)
+            )
         })
     )
+}
 
 // Fetch design system 'principles' directory info from github
-// TODO: Also fetch images folder
 fetch(
     'https://api.github.com/repos/dhis2/design-system/contents/principles?ref=master'
 )
@@ -48,14 +68,9 @@ fetch(
             ({ name, type }) => type === 'file' && name.endsWith('.md')
         )
     )
+    // Fetch files and create local ones for storybook
     .then(downloadAndProcessFiles)
     .then(
         console.log('Design system files downloaded & processed for storybook')
     )
     .catch(console.error)
-
-// TODO: Filter 'download url' and 'name' for: type === 'file' and name ends with '.md'
-// Process name: remove hyphens, toLocaleUppercase
-// Fetch file contents for each of those files
-// Write those contents to markdown files in docs/design-system folder
-// For each file, make a stories.mdx file that imports and renders the
