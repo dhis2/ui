8699b085fb35b69fe4a50252933d4f8cb05429fe
Kai Vandivier <kpvandivier@gmail.com>
2021-03-26 11:20:06 +0100

    docs: improve link transforms on same-page links and md pages

--- docs/fetch-design-system-pages.js
+++ docs/fetch-design-system-pages.js
@@ -5,25 +5,38 @@ const { startCase } = require('lodash')
 const fetch = require('node-fetch')
 
 /**
- * This regex matches relative links in markdown, like '[link](../images/image.png)'
- * There is a negative lookahead assertion to exclude external links that include 'http'
- * The capture groups for the above link would be 1. 'link' and 2. '../images/image.png'
- * Check it out here: https://regex101.com/r/y5o9Tf/1
+ * This regex matches relative links in markdown, like '![link](../images/image.png)'
  *
  * This will be used to transform relative links (and images) in the design system
  * pages into external links to the github repo so these pages don't need to serve
  * images and to enable linking to other design system pages.
+ *
+ * There is a negative lookahead assertion to exclude external links that include 'http'
+ * or same-page links that start with '#'
+ *
+ * It also checks if the link is for an image or not, because they need different handling
+ * The capture groups for the above link would be 1. '!' 2. 'link' and 3. '../images/image.png'
+ *
+ * Check it out here: https://regex101.com/r/y5o9Tf/6
+ *
  */
-const mdLocalLinkRegex = /\[(.*)\]\((?!http)(.*)\)/g
-const designSystemGithubUrl =
+const mdRelativeLinkRegex = /(!)?\[(.*)\]\((?!http|#)(.*)\)/g
+const rawGithubUrl =
     'https://raw.githubusercontent.com/dhis2/design-system/master/principles/'
+const regularGithubUrl =
+    'https://github.com/dhis2/design-system/tree/master/principles'
+
+// eslint-disable-next-line max-params
+const linkReplacer = (_, imageBang, text, linkPath) => {
+    // Different links for images or markdown pages
+    const githubUrl = imageBang ? rawGithubUrl : regularGithubUrl
+    const prefix = imageBang || '' // '!' or ''
+    const url = path.join(githubUrl, linkPath)
+    return `${prefix}[${text}](${url})`
+}
 
 const replaceRelativeLinksWithGithubLinks = text =>
-    text.replaceAll(
-        mdLocalLinkRegex,
-        (_, text, linkPath) =>
-            `[${text}](${path.join(designSystemGithubUrl, linkPath)})`
-    )
+    text.replaceAll(mdRelativeLinkRegex, linkReplacer)
 
 // Template for creating .stories.mdx file for storybook that imports from regular markdown file
 const mdxTemplate = basename => `import { Meta, Description } from '@storybook/addon-docs/blocks'
