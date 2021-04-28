846ca32ed2b259cc248e6bce271f8ce0016313e2
Viktor Varland <viktor@dhis2.org>
2021-04-28 14:01:31 +0200

    refactor: relocate the button to its own package

---
+++ components/button/src/features/can_be_focused/index.js
@@ -0,0 +1,18 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+Given('a Button with onFocus handler is rendered', () => {
+    cy.visitStory('Button', 'With onFocus')
+})
+
+When('the Button is focused', () => {
+    cy.get('[data-test="dhis2-uicore-button"]').focus()
+})
+
+Then('the onFocus handler is called', () => {
+    cy.window().should(win => {
+        expect(win.onFocus).to.be.calledWith({
+            value: 'default',
+            name: 'Button',
+        })
+    })
+})
