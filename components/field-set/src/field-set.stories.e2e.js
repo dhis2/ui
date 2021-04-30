dd2ac46a5200d5d536bc2f316c08838f7a779d1b
Viktor Varland <viktor@dhis2.org>
2021-04-30 12:55:07 +0200

    refactor(field-set): move to components

--- components/field-set/src/field-set.stories.e2e.js
+++ components/field-set/src/field-set.stories.e2e.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { FieldSet } from './FieldSet.js'
+import { FieldSet } from './field-set.js'
 
 storiesOf('FieldSet', module).add('With children', () => (
     <FieldSet>I am a child</FieldSet>
