34d2fa6137a7f4af5fc8907caad9059fb6e9fc55
Viktor Varland <viktor@dhis2.org>
2021-05-03 13:43:29 +0200

    refactor(switch): move to components

--- components/switch/src/switch.js
+++ components/switch/src/switch.js
@@ -2,7 +2,7 @@ import { colors, theme, sharedPropTypes } from '@dhis2/ui-constants'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React, { Component, createRef } from 'react'
-import { SwitchRegular } from '../Icons/index.js'
+import { SwitchRegular } from './switch-icons.js'
 ;('') // TODO: https://github.com/jsdoc/jsdoc/issues/1718
 
 /**
