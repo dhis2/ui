b79c17571ddf8409b64f66ec1e900fda94ed3221
Viktor Varland <viktor@dhis2.org>
2021-05-03 10:43:37 +0200

    refactor(help): move to components

--- components/help/src/help.stories.e2e.js
+++ components/help/src/help.stories.e2e.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { Help } from './Help.js'
+import { Help } from './help.js'
 
 storiesOf('Help', module).add('With children', () => <Help>I am a child</Help>)
