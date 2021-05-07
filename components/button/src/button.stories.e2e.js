b4d9b5273c29e5b54e4cc7ffb2bcfc3b0d9f1916
Viktor Varland <viktor@dhis2.org>
2021-05-07 09:14:23 +0200

    style: fix lint issues

--- components/button/src/button.stories.e2e.js
+++ components/button/src/button.stories.e2e.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import Button from './index.js'
+import { Button } from './button.js'
 
 window.onClick = window.Cypress && window.Cypress.cy.stub()
 window.onBlur = window.Cypress && window.Cypress.cy.stub()
--- components/input/src/input.test.js
+++ components/input/src/input.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import { Input } from '../Input.js'
+import { Input } from './input.js'
 
 describe('<Input>', () => {
     it('passes min, max, and step props as attributes to the native <input> element', () => {
