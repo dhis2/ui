ac0a837266d3db4718fa743dd9703bd4d648177c
Viktor Varland <viktor@dhis2.org>
2021-05-10 14:20:51 +0200

    refactor(organisation-unit-tree): move to components

---
+++ components/organisation-unit-tree/src/__e2e__/children_as_child_nodes.stories.e2e.js
@@ -0,0 +1,23 @@
+import { CustomDataProvider } from '@dhis2/app-runtime'
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import { OrganisationUnitTree } from '../organisation-unit-tree.js'
+import {
+    StatefulMultiSelectionWrapper,
+    dataProviderData,
+    namespace,
+} from './common.js'
+
+storiesOf(namespace, module).add('Closed with children', () => (
+    <CustomDataProvider data={dataProviderData}>
+        <StatefulMultiSelectionWrapper>
+            {({ selected, onChange }) => (
+                <OrganisationUnitTree
+                    roots="A0000000000"
+                    onChange={onChange}
+                    selected={selected}
+                />
+            )}
+        </StatefulMultiSelectionWrapper>
+    </CustomDataProvider>
+))
---
+++ components/organisation-unit-tree/src/__e2e__/force_reload.stories.e2e.js
@@ -0,0 +1,55 @@
+import { CustomDataProvider } from '@dhis2/app-runtime'
+import { storiesOf } from '@storybook/react'
+import React, { useState } from 'react'
+import { OrganisationUnitTree } from '../organisation-unit-tree.js'
+import {
+    StatefulMultiSelectionWrapper,
+    dataProviderData,
+    delayResponse,
+    namespace,
+} from './common.js'
+
+const afterReloadData = {
+    'organisationUnits/A0000000000': delayResponse(
+        1000,
+        dataProviderData['organisationUnits/A0000000000']
+    ),
+    'organisationUnits/A0000000001': delayResponse(
+        2000,
+        dataProviderData['organisationUnits/A0000000001']
+    ),
+}
+
+const ForceReloading = () => {
+    const [forceReload, setForceReload] = useState(false)
+
+    return (
+        <CustomDataProvider data={afterReloadData}>
+            <button
+                disabled={forceReload}
+                data-test="reload-all"
+                onClick={() => setForceReload(true)}
+            >
+                Force reload tree
+            </button>
+
+            <StatefulMultiSelectionWrapper>
+                {({ selected, onChange }) => (
+                    <OrganisationUnitTree
+                        roots="A0000000000"
+                        onChange={onChange}
+                        selected={selected}
+                        forceReload={forceReload}
+                        onChildrenLoaded={data => {
+                            if (data.A0000000000) {
+                                setForceReload(false)
+                            }
+                        }}
+                    />
+                )}
+            </StatefulMultiSelectionWrapper>
+        </CustomDataProvider>
+    )
+}
+
+storiesOf(namespace, module).add('Force reloading', () => <ForceReloading />)
