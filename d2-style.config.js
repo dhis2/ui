4e8e71bb5af6f4a4b0491e8e7650600cd219b9d5
ismay <ismay@ismaywolff.nl>
2021-05-11 17:06:24 +0200

    chore(d2-style): skip files that are ignored for linting

---
+++ d2-style.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    patterns: {
+        js: [
+            '*.{js,jsx,ts,tsx}',
+            '!packages/icons/src/react/**/*.js',
+            '!packages/{widgets,forms}/src/locales/**/*.js',
+        ],
+        text: '*.{md,json,yml,html}',
+    },
+}
