dd96cbcee2cafec747721ccfe602239b5cbde89f
Viktor Varland <viktor@dhis2.org>
2021-05-18 12:36:09 +0200

    refactor(storybook): simplify webpack config mods

--- storybook/src/webpack-config.js
+++ storybook/src/webpack-config.js
@@ -3,9 +3,16 @@ const fg = require('fast-glob')
 const { PROJECT_ROOT, COMPONENTS_DIR, PACKAGES_DIR } = require('./paths.js')
 const { uiPackages } = require('./ui-packages.js')
 
-exports.webpackConfig = async config => {
+/*
+ * Modifies the Wepback loaders
+ *
+ * By default, only src/ is included as a source directory for the
+ * loaders, and since we are using a monorepo we need to add our nested
+ * package source directories to the relevant loaders.
+ */
+function modify_internal_package_loaders(cfg) {
     // Find the rules that configure the webpack loaders
-    const loaderRules = config.module.rules.find(rule => 'oneOf' in rule).oneOf
+    const loaderRules = cfg.module.rules.find(rule => 'oneOf' in rule).oneOf
 
     // Filter only the rules that have a regex under the test property
     const regexLoaders = loaderRules.filter(
@@ -17,6 +24,7 @@ exports.webpackConfig = async config => {
 
     const [components, packages] = uiPackages()
 
+    console.info('custom => Webpack module loaders')
     jsLoaders.forEach(loader => {
         for (const pkg of packages) {
             loader.include.push(new RegExp(`packages/${pkg}/src`))
@@ -25,41 +33,105 @@ exports.webpackConfig = async config => {
         for (const component of components) {
             loader.include.push(new RegExp(`components/${component}/src`))
         }
+
+        console.log(
+            `Loader '${path.relative(
+                process.cwd(),
+                loader.loader
+            )}' matching '${loader.test}' changed to include internal packages`
+        )
     })
 
-    const find_packages = () => {
-        const result = {}
-        const packages = fg.sync([
-            `${COMPONENTS_DIR}/*/package.json`,
-            `${PACKAGES_DIR}/*/package.json`,
-        ])
+    return cfg
+}
 
-        for (const pkg of packages) {
-            const p = require(pkg)
-            const name = p.name
+/*
+ * Modifies the Storybook package resolutions
+ *
+ * We need to find all the packages and components that we want
+ * Storybook to load, and generate resolutions for them, so that we
+ * can redirect the entry-point of a package to the source code and
+ * allow Storybook to compile that on the fly for a smooth
+ * development experience.
+ *
+ * This:
+ *
+ * import { Button } from '@dhis2-ui/button'
+ *
+ * In production resolves to:
+ *
+ * node_modules/@dhis2-ui/button/build/es/index.js
+ *
+ * In development, we intercept and resolve it to:
+ *
+ * node_modules/@dhis2-ui/button/src/index.js
+ *
+ * From there Storybook's Webpack takes over and compiles
+ * src/index.js on-the-fly and injects it into the running code
+ * bundle using fast-refresh in CRA.
+ */
+function modify_internal_package_resolutions(cfg) {
+    const packages = fg.sync([
+        `${COMPONENTS_DIR}/*/package.json`,
+        `${PACKAGES_DIR}/*/package.json`,
+    ])
 
-            const index = fg.sync(`node_modules/${name}/src/**/index.js`, {
-                depth: 1,
-                onlyFiles: true,
-                cwd: PROJECT_ROOT,
-                absolute: true,
-            })
-            result[name] = path.resolve(PROJECT_ROOT, index[0])
+    for (const pkg of packages) {
+        const p = require(pkg)
+        const name = p.name
+
+        const index = fg.sync(`node_modules/${name}/src/**/index.js`, {
+            depth: 1,
+            onlyFiles: true,
+            cwd: PROJECT_ROOT,
+            absolute: true,
+        })
+        cfg.resolve.alias[name] = path.resolve(PROJECT_ROOT, index[0])
+    }
+
+    console.info('custom => Resolve aliases for internal packages:')
+    console.dir(cfg.resolve.alias, { depth: 1 })
+
+    return cfg
+}
+
+/*
+ * Modifies the Webpack plugins used by Storybook.
+ *
+ * ESLint fails on our generated sources (src/locales), and since
+ * most our lint rules are errors, this kills the process and
+ * storybook fails to start, so the ESLintWebpackPlugin needs to be
+ * removed.
+ */
+function modify_webpack_plugins(cfg) {
+    console.info('custom => Disable Webpack plugins')
+
+    // The constructor name for the plugin to remove. Can be found
+    // using: yarn start --debug-webpack
+    const removals = ['ESLintWebpackPlugin']
+
+    console.log('custom => Removed plugin:')
+    for (const removal of removals) {
+        const index = cfg.plugins.findIndex(
+            plugin => plugin.constructor.name === removal
+        )
+
+        if (index === -1) {
+            continue
         }
 
-        console.info('custom => Resolve aliases for internal packages:')
-        console.dir(result, { depth: null })
-        return result
+        const removed = cfg.plugins.splice(index, 1)
+        console.dir(...removed, { depth: 0 })
     }
 
-    return {
-        ...config,
-        resolve: {
-            ...config.resolve,
-            alias: {
-                ...config.resolve.alias,
-                ...find_packages(),
-            },
-        },
-    }
+    return cfg
+}
+
+exports.webpackConfig = async config => {
+    // note: mutates config object
+    modify_internal_package_loaders(config)
+    modify_internal_package_resolutions(config)
+    modify_webpack_plugins(config)
+
+    return config
 }
