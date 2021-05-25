cbc13a58856734c46104eee02cd3e580253c3f0e
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-05-25 18:42:42 +0200

    test(modal): ensure that modal content does not unmount when modal is hidden

--- packages/core/src/Modal/Modal.stories.e2e.js
+++ packages/core/src/Modal/Modal.stories.e2e.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Button,
     ButtonStrip,
@@ -10,6 +10,26 @@ import {
 import { Modal } from './Modal.js'
 
 window.onClose = window.Cypress && window.Cypress.cy.stub()
+
+const StatefuleComponent = () => {
+    const [counter, setCounter] = useState(0)
+
+    return (
+        <div>
+            <p>
+                Current counter:
+                <span id="counter-value">{counter}</span>
+            </p>
+
+            <button
+                id="increment-counter"
+                onClick={() => setCounter(counter + 1)}
+            >
+                Add 1 to counter
+            </button>
+        </div>
+    )
+}
 
 storiesOf('Modal', module)
     .add('With onClose', () => (
@@ -37,3 +57,28 @@ storiesOf('Modal', module)
         </Modal>
     ))
     .add('With children', () => <Modal>I am a child</Modal>)
+    .add('With stateful children', () => {
+        const [hide, setHide] = useState(false)
+
+        return (
+            <div>
+                <button id="show-modal" onClick={() => setHide(false)}>
+                    Show
+                </button>
+
+                <Modal hide={hide}>
+                    <ModalTitle>Can be hidden</ModalTitle>
+
+                    <ModalContent>
+                        <StatefuleComponent />
+                    </ModalContent>
+
+                    <ModalActions>
+                        <button id="hide-modal" onClick={() => setHide(true)}>
+                            Hide modal
+                        </button>
+                    </ModalActions>
+                </Modal>
+            </div>
+        )
+    })
