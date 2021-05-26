2fae69ef942b7b6a230590b7b0562f3a88bbe758
Hendrik de Graaf <hendrik@dhis2.org>
2021-05-26 22:08:23 +0200

    chore(table): restore original core table implementation

---
+++ packages/core/src/Table/TableHead.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+/**
+ * @module
+ * @param {TableHead.PropTypes} props
+ * @returns {React.Component}
+ * @example import { TableHead } from '@dhis2/ui-core'
+ * @see Live demo: {@link /demo/?path=/story/table--static-layout|Storybook}
+ */
+export const TableHead = ({ children, className, dataTest, role }) => (
+    <thead className={className} data-test={dataTest} role={role}>
+        {children}
+    </thead>
+)
+
+TableHead.defaultProps = {
+    dataTest: 'dhis2-uicore-tablehead',
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ * @prop {TableRowHead|Array.<TableRowHead>} [children]
+ * @prop {string} [className]
+ * @prop {string} [role]
+ * @prop {string} [dataTest]
+ */
+TableHead.propTypes = {
+    /** Should be `<TableRowHead>` components */
+    children: PropTypes.node,
+    className: PropTypes.string,
+    dataTest: PropTypes.string,
+    role: PropTypes.string,
+}
