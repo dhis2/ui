1fb14c8780922eead3d4489d6309609936af9dea
Viktor Varland <viktor@dhis2.org>
2021-05-28 12:17:42 +0200

    refactor(storybook): update load-stories function

--- storybook/src/load-stories.js
+++ storybook/src/load-stories.js
@@ -1,5 +1,10 @@
 const path = require('path')
-const { PROJECT_ROOT, COMPONENTS_DIR } = require('./paths.js')
+const {
+    PROJECT_ROOT,
+    COMPONENTS_DIR,
+    UTILITIES_DIR,
+    COLLECTIONS_DIR,
+} = require('./paths.js')
 const { uiPackages } = require('./ui-packages.js')
 
 const isTesting = 'STORYBOOK_TESTING' in process.env
@@ -11,32 +16,68 @@ const isTesting = 'STORYBOOK_TESTING' in process.env
 exports.loadStories = () => {
     const curcomp = path.basename(process.cwd())
 
-    const [components] = uiPackages()
+    const [components, collections, utilities] = uiPackages()
 
     // if we run storybook in one component, we only want to serve a single one
     // and if we run storybook from the project root, we want to load the full shebang.
-    if (components.includes(curcomp)) {
-        return [
-            path.join(
-                COMPONENTS_DIR,
-                curcomp,
-                'src',
-                '**',
-                '*.stories.@(js|jsx|mdx)'
-            ),
-        ]
-    } else {
-        return isTesting
-            ? [
-                  `${PROJECT_ROOT}/collections/*/src/**/*.stories.e2e.@(js|jsx)`,
-                  `${PROJECT_ROOT}/components/*/src/**/*.stories.e2e.@(js|jsx)`,
-                  `${PROJECT_ROOT}/utilities/*/src/**/*.stories.e2e.@(js|jsx)`,
-              ]
-            : [
-                  `${PROJECT_ROOT}/docs/**/*.stories.mdx`,
-                  `${PROJECT_ROOT}/collections/*/src/**/*.stories.@(js|jsx|mdx)`,
-                  `${PROJECT_ROOT}/components/*/src/**/*.stories.@(js|jsx|mdx)`,
-                  `${PROJECT_ROOT}/utilities/*/src/**/*.stories.@(js|jsx|mdx)`,
-              ]
+    switch (true) {
+        case components.includes(curcomp): {
+            console.info(`custom => Loading component stories for '${curcomp}'`)
+            return [
+                path.join(
+                    COMPONENTS_DIR,
+                    curcomp,
+                    'src',
+                    '**',
+                    '*.stories.@(js|jsx|mdx)'
+                ),
+            ]
+        }
+
+        case collections.includes(curcomp): {
+            console.info(
+                `custom => Loading collection stories for '${curcomp}'`
+            )
+            return [
+                path.join(
+                    COLLECTIONS_DIR,
+                    curcomp,
+                    'src',
+                    '**',
+                    '*.stories.@(js|jsx|mdx)'
+                ),
+            ]
+        }
+
+        case utilities.includes(curcomp): {
+            console.info(`custom => Loading utility stories for '${curcomp}'`)
+            return [
+                path.join(
+                    UTILITIES_DIR,
+                    curcomp,
+                    'src',
+                    '**',
+                    '*.stories.@(js|jsx|mdx)'
+                ),
+            ]
+        }
+
+        default: {
+            console.info(
+                `custom => Loading ${isTesting ? 'testing' : 'all'} stories`
+            )
+            return isTesting
+                ? [
+                      `${COLLECTIONS_DIR}/*/src/**/*.stories.e2e.@(js|jsx)`,
+                      `${COMPONENTS_DIR}/*/src/**/*.stories.e2e.@(js|jsx)`,
+                      `${UTILITIES_DIR}/*/src/**/*.stories.e2e.@(js|jsx)`,
+                  ]
+                : [
+                      `${PROJECT_ROOT}/docs/**/*.stories.mdx`,
+                      `${COLLECTIONS_DIR}/*/src/**/*.stories.@(js|jsx|mdx)`,
+                      `${COMPONENTS_DIR}/*/src/**/*.stories.@(js|jsx|mdx)`,
+                      `${UTILITIES_DIR}/*/src/**/*.stories.@(js|jsx|mdx)`,
+                  ]
+        }
     }
 }
