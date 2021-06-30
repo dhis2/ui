48361075b4a2fc6a9095bb05dbb5431b65ebad68
Alexis Rico <sferadev@gmail.com>
2021-06-30 11:34:04 +0200

    Match border color with InputFields

--- components/transfer/src/common/styles.js
+++ components/transfer/src/common/styles.js
@@ -1,5 +1,5 @@
 import { colors } from '@dhis2/ui-constants'
 
 export const backgroundColor = colors.white
-export const borderColor = colors.grey400
+export const borderColor = colors.grey500
 export const borderRadius = '3px'
