1712a034f9acc5e287e07d1aca452e0452dac5c8
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-07-29 11:14:45 +0200

    docs(organisation unit tree): add story for custom node label

--- components/organisation-unit-tree/src/organisation-unit-tree.stories.js
+++ components/organisation-unit-tree/src/organisation-unit-tree.stories.js
@@ -285,6 +285,28 @@ export const MultipleRoots = () => (
 )
 MultipleRoots.storyName = 'Multiple roots'
 
+export const CustomNodeLabel = () => (
+    <OrganisationUnitTree
+        name="Root org unit"
+        roots="A0000000000"
+        initiallyExpanded={['/A0000000000/A0000000001']}
+        renderNodeLabel={data => {
+            if (data.node.path !== '/A0000000000/A0000000001') {
+                return OrganisationUnitTree.defaultProps.renderNodeLabel(data)
+            }
+
+            return OrganisationUnitTree.defaultProps.renderNodeLabel({
+                ...data,
+                node: {
+                    ...data.node,
+                    displayName: <span>--- {data.node.displayName}</span>,
+                },
+            })
+        }}
+        onChange={onChange}
+    />
+)
+
 export const FilteredRoot = () => (
     <OrganisationUnitTree
         onChange={onChange}
