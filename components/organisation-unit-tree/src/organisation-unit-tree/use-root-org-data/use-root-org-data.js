9f52034ffab046ad5eb00ab9e39509ba16e9e678
Hendrik de Graaf <hendrik@dhis2.org>
2021-08-09 15:13:15 +0200

    feat: use filter in query params and a single get request

--- components/organisation-unit-tree/src/organisation-unit-tree/use-root-org-data/use-root-org-data.js
+++ components/organisation-unit-tree/src/organisation-unit-tree/use-root-org-data/use-root-org-data.js
@@ -1,65 +1,43 @@
 import { useDataQuery } from '@dhis2/app-runtime'
-import { useCallback, useState } from 'react'
-import { patchMissingDisplayName } from './patch-missing-display-name.js'
 
-export const createRootQuery = ids =>
-    ids.reduce(
-        (query, id) => ({
-            ...query,
-            [id]: {
-                id,
-                resource: `organisationUnits`,
-                params: ({ isUserDataViewFallback }) => ({
-                    isUserDataViewFallback,
-                    fields: ['displayName', 'path', 'id'],
-                    paging: false,
-                }),
-            },
+const query = {
+    organisationUnits: {
+        resource: 'organisationUnits',
+        params: ({ ids, isUserDataViewFallback }) => ({
+            isUserDataViewFallback,
+            fields: ['displayName', 'path', 'id'],
+            filter: `id:in:[${ids.join()}]`,
         }),
-        {}
-    )
+    },
+}
 
 /**
  * @param {string[]} ids
  * @param {Object} [options]
- * @param {boolean} [options.withChildren]
  * @param {boolean} [options.isUserDataViewFallback]
  * @returns {Object}
  */
 export const useRootOrgData = (ids, { isUserDataViewFallback } = {}) => {
-    const query = createRootQuery(ids)
-    const variables = { isUserDataViewFallback }
-    const [state, setState] = useState({
-        loading: true,
-        error: null,
-        data: null,
+    const { loading, error, data, refetch } = useDataQuery(query, {
+        variables: {
+            ids,
+            isUserDataViewFallback,
+        },
     })
-
-    const { refetch: queryRefetch } = useDataQuery(query, {
-        variables,
-        onComplete: queryData =>
-            setState({
-                ...state,
-                data: queryData ? patchMissingDisplayName(queryData) : {},
-                loading: false,
-            }),
-        onError: queryError =>
-            setState({
-                ...state,
-                error: queryError,
-                loading: false,
-            }),
-    })
-
-    const refetch = useCallback(() => {
-        setState({ ...state, data: null, error: null, loading: true })
-        queryRefetch()
-    }, [queryRefetch, setState])
 
     return {
-        loading: state.loading,
-        error: state.error,
-        data: state.data,
+        loading,
+        error,
+        data: data?.organisationUnits.organisationUnits.reduce(
+            (acc, orgUnit) => {
+                acc[orgUnit.id] = {
+                    ...orgUnit,
+                    displayName: orgUnit.displayName || '',
+                }
+                return acc
+            },
+            {}
+        ),
         refetch,
     }
 }
