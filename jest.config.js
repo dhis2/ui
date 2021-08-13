a0c360bf9460d6bb7f352f6d4c506f47661f0997
Médi-Rémi Hashim <4295266+mediremi@users.noreply.github.com>
2021-08-13 09:27:14 +0100

    test: add test script to each component's package.json file (#687)
    * test: add test script to components' package.json files
    
    * test: use shared jest config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,5 @@ module.exports = {
     rootDir: '.',
     setupFilesAfterEnv: ['<rootDir>/jest.enzyme.config.js'],
     roots: ['<rootDir>/collections', '<rootDir>/components'],
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/build/',
-        '/.d2/',
-        'Transfer/__tests__/common/createChildren.js',
-    ],
+    testPathIgnorePatterns: ['/node_modules/', '/build/', '/.d2/'],
 }
