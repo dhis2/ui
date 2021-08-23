eed305a5afb3a14f030d4a88dabb4eecc06b3d1d
Viktor Varland <viktor@dhis2.org>
2021-08-23 15:24:00 +0200

    docs(portal): add notice about default mount node

--- components/portal/src/portal.js
+++ components/portal/src/portal.js
@@ -2,6 +2,15 @@ import PropTypes from 'prop-types'
 import { useState, useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
+/*
+ * #dhis2-portal-root is provided by the App Platform and will be used by
+ * default.
+ *
+ * For non-platform apps, the mount node can be added manually to the HTML
+ * template.
+ *
+ * As a fallback, portals will be attached to the document body.
+ */
 const defaultNode =
     document.getElementById('dhis2-portal-root') || document.body
 
