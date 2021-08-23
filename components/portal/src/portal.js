1bea993f2fd885e174814441507f782c38541fdd
Viktor Varland <viktor@dhis2.org>
2021-08-23 12:39:03 +0200

    refactor(portal): use portal root from platform
    If the #dhis2-portal-root exists in the DOM, then we use that to mount
    our portals to by default. If it doesn't exist, we fall back to
    document.body.

--- components/portal/src/portal.js
+++ components/portal/src/portal.js
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 import { useState, useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
+const defaultNode =
+    document.getElementById('dhis2-portal-root') || document.body
+
 export const Portal = ({ children, node, disable }) => {
     const [mountNode, setMountNode] = useState(null)
 
     useEffect(() => {
-        setMountNode(node || document.body)
+        setMountNode(node || defaultNode)
     }, [node])
 
     if (disable) {
