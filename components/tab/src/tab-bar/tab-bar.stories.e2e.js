00dda5d90c26dff2d10979798d9ef9508bfd3d5c
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-09-02 12:07:14 +0200

    feat(tab bar): add scroll button auto-hiding feature (LIBS-211)

--- components/tab/src/tab-bar/tab-bar.stories.e2e.js
+++ components/tab/src/tab-bar/tab-bar.stories.e2e.js
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
+import { Tab } from '../tab/index.js'
 import { TabBar } from './index.js'
 
 storiesOf('TabBar', module)
@@ -7,3 +8,25 @@ storiesOf('TabBar', module)
     .add('Scrollable with children', () => (
         <TabBar scrollable>I am a child</TabBar>
     ))
+    .add('Scrollable with some tabs', () => {
+        const TabStaticWidth = () => (
+            <Tab>
+                <div
+                    style={{
+                        width: 100,
+                        border: '1px solid black',
+                    }}
+                >
+                    Foo
+                </div>
+            </Tab>
+        )
+
+        return (
+            <TabBar scrollable>
+                <TabStaticWidth />
+                <TabStaticWidth />
+                <TabStaticWidth />
+            </TabBar>
+        )
+    })
