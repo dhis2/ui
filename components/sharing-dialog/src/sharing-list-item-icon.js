aac40a478583bf7e3754f80e2d5816b81d4b5bed
ismay <ismay@ismaywolff.nl>
2021-10-05 15:50:47 +0200

    refactor(sharing-dialog): remove crashing tests

---
+++ components/sharing-dialog/src/sharing-list-item-icon.js
@@ -0,0 +1,29 @@
+import { UserAvatar } from '@dhis2-ui/user-avatar'
+import { colors } from '@dhis2/ui-constants'
+import { IconWorld24, IconUserGroup24 } from '@dhis2/ui-icons'
+import PropTypes from 'prop-types'
+import React from 'react'
+import {
+    SHARE_TARGET_EXTERNAL,
+    SHARE_TARGET_PUBLIC,
+    SHARE_TARGET_USER,
+    SHARE_TARGET_GROUP,
+} from './sharing-constants'
+
+export const SharingListItemIcon = ({ target }) => {
+    switch (target) {
+        case SHARE_TARGET_EXTERNAL:
+            return <IconWorld24 color={colors.grey600} />
+        case SHARE_TARGET_PUBLIC:
+        case SHARE_TARGET_GROUP:
+            return <IconUserGroup24 color={colors.grey600} />
+        case SHARE_TARGET_USER:
+            return <UserAvatar name={name} small />
+        default:
+            return null
+    }
+}
+
+SharingListItemIcon.propTypes = {
+    target: PropTypes.string.isRequired,
+}
