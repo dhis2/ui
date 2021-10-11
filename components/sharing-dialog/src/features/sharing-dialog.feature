ad8c081b17839981368275a345e3fe5091ff7d2f
ismay <ismay@ismaywolff.nl>
2021-10-11 17:34:07 +0200

    test(sharing-dialog): add access level change test

--- components/sharing-dialog/src/features/sharing-dialog.feature
+++ components/sharing-dialog/src/features/sharing-dialog.feature
@@ -1,5 +1,10 @@
 Feature: Sharing dialog
 
     Scenario: User can change the access level for all users
-        Given a sharing dialog is rendered
-
+        Given a sharing dialog is visible
+        And the all users section is visible
+        And the all users section is labeled no access
+        And the access control is set to no access
+        When the user sets the access level to view only
+        Then the access control is set to view only
+        And the all users section is labeled for view only
--- components/sharing-dialog/src/features/sharing-dialog/sharing-dialog.js
+++ components/sharing-dialog/src/features/sharing-dialog/sharing-dialog.js
@@ -1,6 +1,47 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
-Given('a sharing dialog is rendered', () => {
+Given('a sharing dialog is visible', () => {
     cy.visitStory('sharing-dialog', 'default')
-    cy.contains('Sharing and access').should('exist')
+    cy.contains('Sharing and access').should('be.visible')
+})
+
+Given('the all users section is visible', () => {
+    cy.contains('.share-details-text', 'All users').should('be.visible')
+})
+
+Given('the all users section is labeled no access', () => {
+    cy.contains('.share-details-text', 'No access').should('be.visible')
+})
+
+Given('the access control is set to no access', () => {
+    cy.contains('[data-test="dhis2-uicore-singleselect"]', 'No access').should(
+        'be.visible'
+    )
+})
+
+When('the user sets the access level to view only', () => {
+    cy.contains('[data-test="dhis2-uicore-singleselect"]', 'No access').click()
+    cy.contains(
+        '[data-test="dhis2-uicore-select-menu-menuwrapper"]',
+        'View only'
+    )
+        .should('be.visible')
+        .click()
+
+    // Menu should be closed before continuing
+    cy.contains('[data-test="dhis2-uicore-select-menu-menuwrapper"]').should(
+        'not.exist'
+    )
+})
+
+Then('the access control is set to view only', () => {
+    cy.contains('[data-test="dhis2-uicore-singleselect"]', 'View only').should(
+        'be.visible'
+    )
+})
+
+Given('the all users section is labeled for view only', () => {
+    cy.contains('.share-details-text', 'Anyone logged in can view').should(
+        'be.visible'
+    )
 })
