1a02f92b47d2c800a37c6c9235b501c933f329f8
Kai Vandivier <kpvandivier@gmail.com>
2021-10-12 18:22:15 +0200

    docs(icons): add story description

--- icons/src/icons.stories.js
+++ icons/src/icons.stories.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import * as icons from './react/index.js'
 
+const description = `
+A suite of icons is provided by the UI library; a list is shown below with the
+icon's component name and a demo of the icon. The number in the name refers to
+the size of the icon in px.
+
+Each icon accepts a \`color\` prop that will be applied to the underlying SVG 
+element.
+
+**Example usage:**
+
+\`\`\`jsx
+import { IconAdd16, colors } from '@dhis2/ui'
+
+const IconDemo = () => {
+    return <IconAdd16 color={colors.blue700} />
+}
+\`\`\`
+`
+
 export default {
     title: 'Icons/List',
-    component: icons,
+    component: icons.IconAdd16,
+    parameters: {
+        docs: {
+            description: {
+                component: description,
+            },
+        },
+    },
 }
 
 export const CompleteIconSet = () => (
