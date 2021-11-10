6e68dc544b95007cc019c44f0cfcd3feb4e1f347
Joe Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2021-11-10 08:12:58 +0000

    fix(menu): adjust flyout background elevation and style

--- components/menu/src/flyout-menu/flyout-menu.js
+++ components/menu/src/flyout-menu/flyout-menu.js
@@ -1,8 +1,6 @@
-import { Card } from '@dhis2-ui/card'
-import { spacers } from '@dhis2/ui-constants'
+import { colors, elevations, spacers } from '@dhis2/ui-constants'
 import PropTypes from 'prop-types'
 import React, { Children, cloneElement, isValidElement, useState } from 'react'
-import { resolve } from 'styled-jsx/css'
 import { Menu } from '../index.js'
 
 const FlyoutMenu = ({
@@ -19,39 +17,32 @@ const FlyoutMenu = ({
         setOpenedSubMenu(toggleValue)
     }
 
-    const cardCSS = resolve`
-        padding: ${spacers.dp4} 0;
-        max-height: ${maxHeight};
-        overflow-y: auto;
-    `
-
     return (
         <div className={className} data-test={dataTest}>
-            <Card className={cardCSS.className}>
-                <Menu dense={dense}>
-                    {Children.map(children, (child, index) =>
-                        isValidElement(child)
-                            ? cloneElement(child, {
-                                  showSubMenu: openedSubMenu === index,
-                                  toggleSubMenu: toggleSubMenu.bind(
-                                      this,
-                                      index
-                                  ),
-                              })
-                            : child
-                    )}
-                </Menu>
-            </Card>
+            <Menu dense={dense}>
+                {Children.map(children, (child, index) =>
+                    isValidElement(child)
+                        ? cloneElement(child, {
+                              showSubMenu: openedSubMenu === index,
+                              toggleSubMenu: toggleSubMenu.bind(this, index),
+                          })
+                        : child
+                )}
+            </Menu>
 
             <style jsx>{`
                 div {
+                    background: ${colors.white};
+                    border: 1px solid ${colors.grey200};
+                    border-radius: 3px;
+                    box-shadow: ${elevations.e300};
                     display: inline-block;
                     min-width: ${dense ? '128' : '180'}px;
                     max-width: ${maxWidth};
                     max-height: ${maxHeight};
+                    padding: ${spacers.dp4} 0;
                 }
             `}</style>
-            {cardCSS.styles}
         </div>
     )
 }
