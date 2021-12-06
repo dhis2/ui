41a36716f8c2b347ef787c2baaf63dca295e346f
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-12-06 09:31:59 +0100

    feat(dropdown-button): add controlled mode (#865)
    * feat(dropdown-button): add controlled mode
    
    * test(button): use mount and workaround for popper
    
    A shallow render is not enough for the DropdownComponent, so by using
    mount instead we don't have to use the default object in the
    controlledOnClick function.
    
    That reveals a second problem that is documented on the react-popper
    issue tracker[1].
    
    Basically, we need to flush the micro-task queue for Promises to make
    sure that the async functions in Popper have completed and the render is
    actually completed.
    
    We do this by using the trick:
    
        await act(async () => await null)
    
    after rendering the component that has a nested Popper.
    
    [1] https://github.com/popperjs/react-popper/issues/350
    
    * refactor(dropdown-button): single click handler
    
    Merges controlledOnClick and toggleOpenState into one handler that does
    the correct action based on if the component is controlled or
    uncontrolled.
    
    Moves the isControlled helper to a class property so it can be used in
    the handler and the render function.
    
    Makes the onClick prop optional in all cases, and simplifies the
    controlled state to depend solely on the 'open' prop.
    
    Changes the controlled DropdownButton story to be based on the open
    prop without state management so that there is a clear difference in
    behaviour across the uncontrolled/controlled stories.
    
    * refactor(dropdown-button): reintroduce requiredIf
    
    Simplify internals and rules for when to use the props.open vs.
    state.open.
    
    * chore(dropdown-button): add open and manual control stories
    
    * refactor(dropdown-button): split state/prop click handling
    
    Co-authored-by: Viktor Varland <viktor@dhis2.org>

--- components/button/src/dropdown-button/dropdown-button.stories.js
+++ components/button/src/dropdown-button/dropdown-button.stories.js
@@ -1,6 +1,6 @@
 import { FlyoutMenu, MenuItem } from '@dhis2-ui/menu'
 import { sharedPropTypes } from '@dhis2/ui-constants'
-import React from 'react'
+import React, { useState } from 'react'
 import { DropdownButton } from './index.js'
 
 const description = `
@@ -10,13 +10,6 @@ Presents several actions to a user in a small space. Can replace single, individ
 import { DropdownButton } from '@dhis2/ui'
 \`\`\`
 `
-
-window.onClick = (payload, event) => {
-    console.log('onClick payload', payload)
-    console.log('onClick event', event)
-}
-
-const onClick = (...args) => window.onClick(...args)
 
 const Simple = <span>Simplest thing</span>
 
@@ -47,7 +40,9 @@ const Template = (args) => <DropdownButton {...args} />
 export const Default = Template.bind({})
 
 export const WithClick = Template.bind({})
-WithClick.args = { onClick: onClick }
+WithClick.args = {
+    onClick: ({ open }) => console.log('onClick: the dropdown is open: ', open),
+}
 
 export const Primary = Template.bind({})
 Primary.args = { primary: true }
@@ -89,3 +84,37 @@ InitialFocus.args = { initialFocus: true }
  * not the normal 'canvas' story viewer)
  */
 InitialFocus.parameters = { docs: { disable: true } }
+
+const OpenTemplate = (args) => {
+    return <DropdownButton {...args} />
+}
+export const Open = OpenTemplate.bind({})
+Open.args = {
+    open: true,
+    component: Simple,
+    onClick: () => {},
+}
+
+const ManualControlTemplate = (args) => {
+    const [isOpen, setIsOpen] = useState(true)
+
+    const handleOpen = () => setIsOpen(false)
+
+    const Menu = (
+        <ul>
+            <li>First option does nothing</li>
+            <li onClick={handleOpen}>Close the dropdown</li>
+        </ul>
+    )
+
+    return (
+        <DropdownButton
+            {...args}
+            onClick={() => setIsOpen(!isOpen)}
+            open={isOpen}
+            component={Menu}
+        />
+    )
+}
+export const ManualControl = ManualControlTemplate.bind({})
+ManualControl.args = {}
