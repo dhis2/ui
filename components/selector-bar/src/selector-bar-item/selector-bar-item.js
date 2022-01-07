4268dae0739846ae97bbacbf358bed4e32e37116
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-01-07 14:47:31 +0100

    feat(selector bar item): add tooltipContent prop

--- components/selector-bar/src/selector-bar-item/selector-bar-item.js
+++ components/selector-bar/src/selector-bar-item/selector-bar-item.js
@@ -1,6 +1,7 @@
 import { Card } from '@dhis2-ui/card'
 import { Layer } from '@dhis2-ui/layer'
 import { Popper } from '@dhis2-ui/popper'
+import { Tooltip } from '@dhis2-ui/tooltip'
 import { colors, spacers } from '@dhis2/ui-constants'
 import { IconChevronUp24, IconChevronDown24 } from '@dhis2/ui-icons'
 import PropTypes from 'prop-types'
@@ -11,6 +12,31 @@ const offsetModifier = {
     options: {
         offset: [0, 2],
     },
+}
+
+const SelectorBarItemTooltip = ({ tooltipContent, children }) => {
+    if (!tooltipContent) {
+        return children
+    }
+
+    return (
+        <Tooltip content={tooltipContent}>
+            {({ ref, onMouseOver, onMouseOut }) => (
+                <div
+                    ref={ref}
+                    onMouseOver={onMouseOver}
+                    onMouseOut={onMouseOut}
+                >
+                    {children}
+                </div>
+            )}
+        </Tooltip>
+    )
+}
+
+SelectorBarItemTooltip.propTypes = {
+    children: PropTypes.any.isRequired,
+    tooltipContent: PropTypes.string,
 }
 
 /**
@@ -30,85 +56,88 @@ export const SelectorBarItem = ({
     disabled,
     label,
     value,
+    tooltipContent,
     noValueMessage,
 }) => {
     const buttonRef = useRef()
     const Icon = open ? IconChevronUp24 : IconChevronDown24
 
     return (
-        <button
-            ref={buttonRef}
-            className="selector-bar-item"
-            disabled={disabled}
-            onClick={() => setOpen(true)}
-        >
-            <span className="label">{label}</span>
+        <SelectorBarItemTooltip tooltipContent={tooltipContent}>
+            <button
+                ref={buttonRef}
+                className="selector-bar-item"
+                disabled={disabled}
+                onClick={() => setOpen(true)}
+            >
+                <span className="label">{label}</span>
 
-            {!disabled && (
-                <span className="value">{value || noValueMessage}</span>
-            )}
+                {!disabled && (
+                    <span className="value">{value || noValueMessage}</span>
+                )}
 
-            <span className="toggle-icon">
-                <Icon />
-            </span>
+                <span className="toggle-icon">
+                    <Icon />
+                </span>
 
-            {open && (
-                <Layer
-                    onClick={(_, evt) => {
-                        evt.stopPropagation()
-                        setOpen(false)
-                    }}
-                >
-                    <Popper
-                        reference={buttonRef}
-                        placement="bottom-end"
-                        modifiers={[offsetModifier]}
+                {open && (
+                    <Layer
+                        onClick={(_, evt) => {
+                            evt.stopPropagation()
+                            setOpen(false)
+                        }}
                     >
-                        <Card>{children}</Card>
-                    </Popper>
-                </Layer>
-            )}
+                        <Popper
+                            reference={buttonRef}
+                            placement="bottom-end"
+                            modifiers={[offsetModifier]}
+                        >
+                            <Card>{children}</Card>
+                        </Popper>
+                    </Layer>
+                )}
 
-            <style jsx>{`
-                .selector-bar-item {
-                    display: flex;
-                    cursor: pointer;
-                    background: none;
-                    height: 40px;
-                    align-items: center;
-                    ${
-                        /*
-                         * The arrow icon has ~4px empty space on the sides,
-                         * that's why the padding on the right is only 8px.
-                         */ ''
+                <style jsx>{`
+                    .selector-bar-item {
+                        display: flex;
+                        cursor: pointer;
+                        background: none;
+                        height: 40px;
+                        align-items: center;
+                        ${
+                            /*
+                             * The arrow icon has ~4px empty space on the sides,
+                             * that's why the padding on the right is only 8px.
+                             */ ''
+                        }
+                        padding: 0 ${spacers.dp8} 0 ${spacers.dp12};
+                        font-size: 14px;
+                        line-height: 16px;
+                        border: none;
+                        box-shadow: 0px 0px 0px 1px ${colors.grey400};
                     }
-                    padding: 0 ${spacers.dp8} 0 ${spacers.dp12};
-                    font-size: 14px;
-                    line-height: 16px;
-                    border: none;
-                    box-shadow: 0px 0px 0px 1px ${colors.grey400};
-                }
 
-                .selector-bar-item:disabled {
-                    cursor: not-allowed;
-                }
+                    .selector-bar-item:disabled {
+                        cursor: not-allowed;
+                    }
 
-                .label {
-                    color: ${colors.grey600};
-                }
+                    .label {
+                        color: ${colors.grey600};
+                    }
 
-                .value {
-                    padding-left: ${spacers.dp8};
-                }
+                    .value {
+                        padding-left: ${spacers.dp8};
+                    }
 
-                .toggle-icon {
-                    display: flex;
-                    margin-left: ${spacers.dp4};
-                    height: 100%;
-                    align-items: center;
-                }
-            `}</style>
-        </button>
+                    .toggle-icon {
+                        display: flex;
+                        margin-left: ${spacers.dp4};
+                        height: 100%;
+                        align-items: center;
+                    }
+                `}</style>
+            </button>
+        </SelectorBarItemTooltip>
     )
 }
 
@@ -119,5 +148,6 @@ SelectorBarItem.propTypes = {
     open: PropTypes.bool.isRequired,
     setOpen: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
+    tooltipContent: PropTypes.any,
     value: PropTypes.string,
 }
