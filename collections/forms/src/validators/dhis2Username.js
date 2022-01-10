48c1c46563c66f69b520d45b090e6de970b00bd3
Médi-Rémi Hashim <mediremi@gmail.com>
2022-01-10 18:08:14 +0000

    fix(forms): update username regex to match 2.38 backend

--- collections/forms/src/validators/dhis2Username.js
+++ collections/forms/src/validators/dhis2Username.js
@@ -1,13 +1,15 @@
 import i18n from '../locales/index.js'
 import { isEmpty, isString } from './helpers/index.js'
 
+const USERNAME_PATTERN =
+    /^(?=.{4,255}$)(?![_.@])(?!.*[_.@]{2})[a-z0-9._@]+(?<![_.@])$/
+
 const invalidUsernameMessage = i18n.t(
-    'Please provide a username between 1 and 255 characters'
+    'Please provide a username between 4 and 255 characters'
 )
 
 const dhis2Username = (value) =>
-    isEmpty(value) ||
-    (isString(value) && value.length >= 1 && value.length <= 255)
+    isEmpty(value) || (isString(value) && USERNAME_PATTERN.test(value))
         ? undefined
         : invalidUsernameMessage
 
