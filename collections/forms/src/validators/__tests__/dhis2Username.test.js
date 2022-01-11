f497bb0c290e71725d66a5db9cd8f93bec46622a
Médi-Rémi Hashim <4295266+mediremi@users.noreply.github.com>
2022-01-11 08:03:20 +0000

    fix(forms): update username regex to match 2.38 backend (#914)

--- collections/forms/src/validators/__tests__/dhis2Username.test.js
+++ collections/forms/src/validators/__tests__/dhis2Username.test.js
@@ -7,29 +7,63 @@ import {
 describe('validator: dhis2Username', () => {
     allowsEmptyValues(dhis2Username)
 
-    describe('allows all sorts of strings between 1 and 255 characters long', () => {
+    describe('constrains length of username to between 4 and 255 characters long', () => {
         testValidatorValues(dhis2Username, undefined, [
-            'electricchicken',
-            '1', //1
-            'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss', //255
-            'some_username^%&*(',
-            'あいうえお',
+            's'.repeat(4),
+            's'.repeat(255),
+            'valid_username',
+        ])
+
+        testValidatorValues(dhis2Username, invalidUsernameMessage, [
+            '1',
+            's',
+            's'.repeat(256),
         ])
     })
 
-    describe('rejects other data types', () => {
+    describe('does not allow usernames to start with _, . or @', () => {
+        testValidatorValues(dhis2Username, invalidUsernameMessage, [
+            '_xxx',
+            '.xxx',
+            '@xxx',
+        ])
+    })
+
+    describe('does not allow usernames to end with _, . or @', () => {
+        testValidatorValues(dhis2Username, invalidUsernameMessage, [
+            'xxx_',
+            'xxx.',
+            'xxx@',
+        ])
+    })
+
+    describe('does not allow usernames to contain __, .. or @@', () => {
+        testValidatorValues(dhis2Username, invalidUsernameMessage, [
+            '__xx',
+            '..xx',
+            '@@xx',
+        ])
+    })
+
+    describe('constrains characters in usernames to [a-z0-9._@]', () => {
+        testValidatorValues(dhis2Username, undefined, [
+            'v@lid_user.name',
+            '123another_v@lid_usern@me',
+        ])
+
+        testValidatorValues(dhis2Username, invalidUsernameMessage, [
+            'あいうえお',
+            'some_username^%&*(',
+        ])
+    })
+
+    describe('rejects non-string data types', () => {
         testValidatorValues(dhis2Username, invalidUsernameMessage, [
             1,
             true,
             {},
             [],
             () => {},
-        ])
-    })
-
-    describe('values that are too long', () => {
-        testValidatorValues(dhis2Username, invalidUsernameMessage, [
-            'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss', //256
         ])
     })
 })
