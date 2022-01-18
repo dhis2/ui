334bbe39efe4faaf4e9cb03bef1fa08f8718bea9
Viktor Varland <viktor@dhis2.org>
2022-01-18 11:06:06 +0100

    refactor(docusaurus): clean up docs-folder

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,115 @@
+const { webpackConfig } = require('../storybook/src/webpack-config.js')
+
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'DHIS2 UI',
+  tagline: 'React Components for DHIS2 Web Applications',
+  url: 'https://ui.dhis2.nu',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'dhis2', // Usually your GitHub org/user name.
+  projectName: 'ui', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/dhis2/ui',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+    plugins: [
+        function customWebpack(context, options) {
+            return {
+                name: 'custom-webpack',
+                configureWebpack(config, isServer, utils) {
+                    return webpackConfig(config)
+                },
+            }
+        },
+    ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'DHIS2 UI',
+        logo: {
+          alt: 'DHIS2 UI Logotype',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'getting-started',
+            position: 'left',
+            label: 'Getting started',
+          },
+          {
+            href: 'https://github.com/dhis2/ui',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting started',
+                to: '/docs/getting-started',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'DHIS2 Community of Practice',
+                href: 'https://community.dhis2.org/tag/ui',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
