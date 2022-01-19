99e9772e09d6ed3692ec5236f9412760fcb3b303
Joe Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2022-01-19 11:00:49 +0100

    docs: demo component style

--- docs/DemoComponent.jsx
+++ docs/DemoComponent.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 export const Demo = (props) => {
     return (
-        <div>
-            {props.children}
+        <div className="demo">
+            <div className="demo-title">Demo</div>
+            <div className="demo-content">{props.children}</div>
+            <style>
+                {`
+                    .demo {
+                        border: 1px solid #e5e5e5;
+                        margin-bottom: 16px;
+                    }
+                    .demo-title {
+                        width: 100%;
+                        background-color: #e5e5e5;
+                        font-size: 0.8rem;
+                        padding: 2px 4px;
+                        font-family: monospace;
+                    }
+                    .demo-content {
+                        padding: 24px 16px;
+                    }
+                `}
+            </style>
         </div>
     )
 }
