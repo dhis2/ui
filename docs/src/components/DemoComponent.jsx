61fa6ee84dca313ef229a1bc5ce1aff55d9c1627
Joe Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2022-02-10 14:32:14 +0100

    docs: remove iframe from demo component

--- docs/src/components/DemoComponent.jsx
+++ docs/src/components/DemoComponent.jsx
@@ -1,63 +1,16 @@
-import clsx from 'clsx'
-import React, { useState, useEffect } from 'react'
-import { createPortal } from 'react-dom'
+import PropTypes from 'prop-types'
+import React from 'react'
 import styles from './DemoComponent.module.css'
 
 export const Demo = (props) => {
-    const [styleLoading, setStyleLoading] = useState(true)
-    const [frameLoading, setFrameLoading] = useState(true)
-    const [contentRef, setContentRef] = useState(null)
-    const [mountNode, setMountNode] = useState(null)
-
-    useEffect(() => {
-        const styleTag = document.querySelector('[data-styled-jsx]')
-
-        setMountNode(contentRef?.contentWindow?.document?.body)
-
-        const mountHead = contentRef?.contentWindow?.document?.head
-
-        if (mountHead && styleTag) {
-            for (const stylesheet of document.styleSheets) {
-                const iframeDoc = contentRef?.contentWindow?.document
-                const style = iframeDoc.createElement('style')
-
-                style.appendChild(iframeDoc.createTextNode(''))
-
-                mountHead.appendChild(style)
-
-                if (stylesheet.ownerNode === styleTag) {
-                    for (const rule of stylesheet.cssRules) {
-                        // Good debugging entry-point for figuring out which
-                        // styles are injected into the iframe.
-                        // console.log(rule)
-                        style.sheet.insertRule(rule.cssText)
-                    }
-                }
-                style.sheet.insertRule(
-                    'body { font-family: "Roboto", sans-serif; }'
-                )
-            }
-
-            mountHead.appendChild(styleTag.cloneNode())
-            setStyleLoading(false)
-        }
-    })
-
     return (
         <div className={styles.demo}>
             <div className={styles.demoTitle}>Demo</div>
-            <iframe
-                loading="lazy"
-                onLoad={(e) => setFrameLoading(false)}
-                className={clsx(
-                    styles.demoFrame,
-                    styleLoading ? styles.hidden : styles.visible
-                )}
-                {...props}
-                ref={setContentRef}
-            >
-                {mountNode && createPortal(props.children, mountNode)}
-            </iframe>
+            {/* eslint-disable-next-line react/prop-types */}
+            <div className={styles.demoFrame}>{props.children}</div>
         </div>
     )
+}
+Demo.PropTypes = {
+    children: PropTypes.node,
 }
