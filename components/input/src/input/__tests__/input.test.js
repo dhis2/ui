abb6d9709bee6f70f8b3ec0e7f067a071da639f4
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-05-25 11:23:21 +0200

    test(input): add jest test for onKeyDown prop

---
+++ components/input/src/input/__tests__/input.test.js
@@ -0,0 +1,35 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import { shallow } from 'enzyme'
+import React from 'react'
+import { Input } from '../input.js'
+
+describe('<Input>', () => {
+    it('passes min, max, and step props as attributes to the native <input> element', () => {
+        const testProps = { min: '0', max: '10', step: '0.5' }
+        const wrapper = shallow(<Input type="number" {...testProps} />)
+
+        const inputEl = wrapper.find('input')
+        expect(inputEl.props()).toMatchObject(testProps)
+    })
+
+    it('should call the onKeyDown callback when provided', () => {
+        const onKeyDown = jest.fn()
+
+        render(
+            <Input
+                name="foo"
+                value="bar"
+                onKeyDown={onKeyDown}
+            />
+        )
+
+        fireEvent.keyDown(screen.getByRole('textbox'), {})
+
+        expect(onKeyDown).toHaveBeenCalledWith(
+            { name: 'foo', value: 'bar' },
+            expect.objectContaining({})
+        )
+
+        expect(onKeyDown).toHaveBeenCalledTimes(1)
+    })
+})
