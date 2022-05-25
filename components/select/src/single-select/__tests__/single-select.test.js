d62f036034ae751fb658a80168a911caa42d6d20
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-05-25 11:24:47 +0200

    test(single select): add jest test for onKeyDown prop

---
+++ components/select/src/single-select/__tests__/single-select.test.js
@@ -0,0 +1,28 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import React from 'react'
+import { SingleSelect } from '../single-select.js'
+
+describe('<SingleSelect />', () => {
+    it('should call the onKeyDown callback when provided', () => {
+        const onKeyDown = jest.fn()
+
+        render(
+            <SingleSelect
+                placeholder="placeholder"
+                onKeyDown={onKeyDown}
+            />
+        )
+
+        fireEvent.keyDown(
+            screen.getByTestId('dhis2-uicore-singleselect-placeholder'),
+            { key: 'Enter', code: 'Enter', charCode: 13 }
+        )
+
+        expect(onKeyDown).toHaveBeenCalledWith(
+            { selected: '' },
+            expect.objectContaining({})
+        )
+
+        expect(onKeyDown).toHaveBeenCalledTimes(1)
+    })
+})
