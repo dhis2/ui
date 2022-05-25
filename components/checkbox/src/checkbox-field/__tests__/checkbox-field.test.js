cc59797a9668dd416a1614d3385d7fbd557a8ffc
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-05-25 11:21:09 +0200

    test(checkbox field): add jest test for onKeyDown prop

---
+++ components/checkbox/src/checkbox-field/__tests__/checkbox-field.test.js
@@ -0,0 +1,31 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import React from 'react'
+import { CheckboxField } from '../checkbox-field.js'
+
+describe('<CheckboxField />', () => {
+    it('should call the onKeyDown callback when provided', () => {
+        const onKeyDown = jest.fn()
+
+        render(
+            <CheckboxField
+                label="Label"
+                name="foo"
+                value="bar"
+                checked={false}
+                onKeyDown={onKeyDown}
+            />
+        )
+
+        fireEvent.keyDown(
+            screen.getByRole('checkbox'),
+            { key: 'Enter', code: 'Enter', charCode: 13 }
+        )
+
+        expect(onKeyDown).toHaveBeenCalledWith(
+            { name: 'foo', value: 'bar', checked: true },
+            expect.objectContaining({})
+        )
+
+        expect(onKeyDown).toHaveBeenCalledTimes(1)
+    })
+})
