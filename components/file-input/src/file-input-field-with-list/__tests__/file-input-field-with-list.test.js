ce7f6c17100ff2d7618b7dece8dafd0b36bfcec1
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-05-25 11:22:28 +0200

    test(file input field with list): add jest test for onKeyDown prop

---
+++ components/file-input/src/file-input-field-with-list/__tests__/file-input-field-with-list.test.js
@@ -0,0 +1,31 @@
+import { render, fireEvent, screen } from '@testing-library/react'
+import React from 'react'
+import { FileInputFieldWithList } from '../file-input-field-with-list.js'
+
+describe('<FileInputFieldWithList />', () => {
+
+    it('should call the onKeyDown callback when provided', () => {
+        const onKeyDown = jest.fn()
+
+        render(
+            <FileInputFieldWithList
+                label="Label"
+                name="foo"
+                value="bar"
+                checked={false}
+                onKeyDown={onKeyDown}
+                onChange={jest.fn()}
+            />
+        )
+
+        fireEvent.keyDown(screen.getByRole('button'), {})
+
+        expect(onKeyDown).toHaveBeenCalledTimes(1)
+
+        const input = screen.getByTestId('dhis2-uicore-fileinput-input')
+        expect(onKeyDown).toHaveBeenCalledWith(
+            { name: 'foo', files: input.files },
+            expect.objectContaining({})
+        )
+    })
+})
