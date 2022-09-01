116978da376de2004dd0f7e0af160367e1d0bb66
ismay <git.o8qty@simplelogin.com>
2022-09-01 10:37:06 +0200

    test(transfer): add mount test with package import

--- components/transfer/src/__tests__/imports.test.js
+++ components/transfer/src/__tests__/imports.test.js
@@ -1,9 +1,14 @@
+import { Transfer as PackageTransfer } from '@dhis2-ui/transfer'
 import { mount } from 'enzyme'
 import React from 'react'
-import { Transfer } from '../index.js'
+import { Transfer as LocalTransfer } from '../index.js'
 
 describe('<Transfer />', () => {
-    it('should be able to mount the transfer without errors', () => {
-        mount(<Transfer onChange={() => {}} options={[]} />)
+    it('should be able to mount the local transfer without errors', () => {
+        mount(<LocalTransfer onChange={() => {}} options={[]} />)
+    })
+
+    it('should be able to mount the package transfer without errors', () => {
+        mount(<PackageTransfer onChange={() => {}} options={[]} />)
     })
 })
