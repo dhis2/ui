ae313982367190c7cb67e8ade95bbbe15362ff08
Joseph John Aas Cooper <joe@dhis2.org>
2023-06-28 10:02:57 +0200

    fix(input): force maximum height

--- components/input/src/input/input.js
+++ components/input/src/input/input.js
@@ -22,7 +22,7 @@ const styles = css`
         color: ${colors.grey900};
         background-color: white;
 
-        padding: 12px 11px 10px;
+        max-height: 40px;
 
         outline: 0;
         border: 1px solid ${colors.grey500};
@@ -32,7 +32,7 @@ const styles = css`
     }
 
     input.dense {
-        padding: 8px 11px 6px;
+        max-height: 32px;
     }
 
     input:focus {
