37ce06381be9f72fb6ba4d6d96fa2b42c2ad0155
Birk Johansson <birk@dhis2.org>
2023-09-06 23:42:08 +0200

    style: prettier constants decl

--- constants/index.d.ts
+++ constants/index.d.ts
@@ -1,12 +1,21 @@
+export type ColorBase = 'blue' | 'teal' | 'red' | 'yellow' | 'green' | 'grey'
+export type ColorVariant =
+    | '900'
+    | '800'
+    | '700'
+    | '600'
+    | '500'
+    | '400'
+    | '300'
+    | '200'
+    | '100'
+    | '050'
+export type ColorProp = `${ColorBase}${ColorVariant}` | 'white'
 
-export type ColorBase = 'blue' | 'teal' | 'red' | 'yellow' | 'green' | 'grey';
-export type ColorVariant = '900' | '800' | '700' | '600' | '500' | '400' | '300' | '200' | '100' | '050';
-export type ColorProp = `${ColorBase}${ColorVariant}` | 'white';
+export const colors: Record<ColorProp, string>
 
-export const colors: Record<ColorProp, string>;
-
-export type ElevantionVariant = 'e100' | 'e200' | 'e300' | 'e400';
-export const elevations: Record<ElevantionVariant, string>;
+export type ElevantionVariant = 'e100' | 'e200' | 'e300' | 'e400'
+export const elevations: Record<ElevantionVariant, string>
 
 export type SpacerVariant =
     | 'dp4'
@@ -23,15 +32,25 @@ export type SpacerVariant =
     | 'dp256'
     | 'dp384'
     | 'dp512'
-    | 'dp640';
-export const spacersNum: Record<SpacerVariant, number>;
-export const spacers: Record<SpacerVariant, string>;
+    | 'dp640'
+export const spacersNum: Record<SpacerVariant, number>
+export const spacers: Record<SpacerVariant, string>
 
-export type LayerVariant = 'applicationTop' | 'blocking' | 'alert';
-export const layer: Record<LayerVariant, number>;
+export type LayerVariant = 'applicationTop' | 'blocking' | 'alert'
+export const layer: Record<LayerVariant, number>
 
-export type ThemeBase = 'primary' | 'secondary';
-export type ThemeVariant = '900' | '800' | '700' | '600' | '500' | '400' | '300' | '200' | '100' | '050';
+export type ThemeBase = 'primary' | 'secondary'
+export type ThemeVariant =
+    | '900'
+    | '800'
+    | '700'
+    | '600'
+    | '500'
+    | '400'
+    | '300'
+    | '200'
+    | '100'
+    | '050'
 export type ThemeProp =
     | `${ThemeBase}${ThemeVariant}`
     | 'default'
@@ -40,5 +59,5 @@ export type ThemeProp =
     | 'warning'
     | 'disabled'
     | 'focus'
-    | 'fonts';
-export const theme: Record<ThemeProp, string>;
+    | 'fonts'
+export const theme: Record<ThemeProp, string>
