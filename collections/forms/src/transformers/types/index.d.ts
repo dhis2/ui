e3d27f8e200067976559d31dcd035ca4fa1103cb
Birk Johansson <birk@dhis2.org>
2023-10-16 22:24:12 +0200

    fix: remove old-types structure

--- collections/forms/src/transformers/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-
-type ValuesWithId = Array<{ id: string }> | undefined
-
-export const arrayWithIdObjects: {
-    format: (value: ValuesWithId) => string,
-    pars: (value: string) => ValuesWithId
-}
