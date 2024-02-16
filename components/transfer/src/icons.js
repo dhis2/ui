c09a69c47b8d9c0908ec79fee87a485853111a3d
Thomas Zemp <tom@dhis2.org>
2024-02-16 15:15:08 +0100

    fix: transfer icons style placement

--- components/transfer/src/icons.js
+++ components/transfer/src/icons.js
@@ -13,31 +13,29 @@ const centerButtonStyles = css`
 `
 
 export const IconAddAll = ({ dataTest, disabled }) => (
-    <>
-        <div className="centerButton">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                data-test={dataTest}
-                fill={disabled ? theme.disabled : '#404B5A'}
-            >
-                <g fillRule="evenodd">
-                    <polygon
-                        fillRule="nonzero"
-                        points="6 2 4.94 3.06 9.13 7.25 0 7.25 0 8.75 9.13 8.75 4.94 12.94 6 14 12 8"
-                    />
+    <div className="centerButton">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            data-test={dataTest}
+            fill={disabled ? theme.disabled : '#404B5A'}
+        >
+            <g fillRule="evenodd">
+                <polygon
+                    fillRule="nonzero"
+                    points="6 2 4.94 3.06 9.13 7.25 0 7.25 0 8.75 9.13 8.75 4.94 12.94 6 14 12 8"
+                />
 
-                    <polygon
-                        fillRule="nonzero"
-                        points="10 2 8.94 3.06 13.13 7.25 13.9 8 13.13 8.75 8.94 12.94 10 14 16 8"
-                    />
-                </g>
-            </svg>
-        </div>
+                <polygon
+                    fillRule="nonzero"
+                    points="10 2 8.94 3.06 13.13 7.25 13.9 8 13.13 8.75 8.94 12.94 10 14 16 8"
+                />
+            </g>
+        </svg>
         <style jsx>{centerButtonStyles}</style>
-    </>
+    </div>
 )
 
 IconAddAll.propTypes = {
@@ -46,21 +44,19 @@ IconAddAll.propTypes = {
 }
 
 export const IconAddIndividual = ({ dataTest, disabled }) => (
-    <>
-        <div className="centerButton">
-            <svg
-                fill={disabled ? theme.disabled : '#404B5A'}
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                data-test={dataTest}
-            >
-                <polygon points="8 2 6.94 3.06 11.13 7.25 2 7.25 2 8.75 11.13 8.75 6.94 12.94 8 14 14 8" />
-            </svg>
-        </div>
+    <div className="centerButton">
+        <svg
+            fill={disabled ? theme.disabled : '#404B5A'}
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            data-test={dataTest}
+        >
+            <polygon points="8 2 6.94 3.06 11.13 7.25 2 7.25 2 8.75 11.13 8.75 6.94 12.94 8 14 14 8" />
+        </svg>
         <style jsx>{centerButtonStyles}</style>
-    </>
+    </div>
 )
 
 IconAddIndividual.propTypes = {
@@ -69,33 +65,31 @@ IconAddIndividual.propTypes = {
 }
 
 export const IconRemoveAll = ({ dataTest, disabled }) => (
-    <>
-        <div className="centerButton">
-            <svg
-                fill={disabled ? theme.disabled : '#404B5A'}
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-                data-test={dataTest}
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <g fillRule="evenodd">
-                    <polygon
-                        fillRule="nonzero"
-                        points="10 2 8.94 3.06 13.13 7.25 4 7.25 4 8.75 13.13 8.75 8.94 12.94 10 14 16 8"
-                        transform="matrix(-1 0 0 1 20 0)"
-                    />
+    <div className="centerButton">
+        <svg
+            fill={disabled ? theme.disabled : '#404B5A'}
+            width="16px"
+            height="16px"
+            viewBox="0 0 16 16"
+            data-test={dataTest}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g fillRule="evenodd">
+                <polygon
+                    fillRule="nonzero"
+                    points="10 2 8.94 3.06 13.13 7.25 4 7.25 4 8.75 13.13 8.75 8.94 12.94 10 14 16 8"
+                    transform="matrix(-1 0 0 1 20 0)"
+                />
 
-                    <polygon
-                        fillRule="nonzero"
-                        points="1.06 2 0 3.06 4.19 7.25 4.96 8 4.19 8.75 0 12.94 1.06 14 7.06 8"
-                        transform="matrix(-1 0 0 1 7.06 0)"
-                    />
-                </g>
-            </svg>
-        </div>
+                <polygon
+                    fillRule="nonzero"
+                    points="1.06 2 0 3.06 4.19 7.25 4.96 8 4.19 8.75 0 12.94 1.06 14 7.06 8"
+                    transform="matrix(-1 0 0 1 7.06 0)"
+                />
+            </g>
+        </svg>
         <style jsx>{centerButtonStyles}</style>
-    </>
+    </div>
 )
 
 IconRemoveAll.propTypes = {
@@ -104,24 +98,22 @@ IconRemoveAll.propTypes = {
 }
 
 export const IconRemoveIndividual = ({ dataTest, disabled }) => (
-    <>
-        <div className="centerButton">
-            <svg
-                fill={disabled ? theme.disabled : '#404B5A'}
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                data-test={dataTest}
-            >
-                <polygon
-                    points="8 2 6.94 3.06 11.13 7.25 2 7.25 2 8.75 11.13 8.75 6.94 12.94 8 14 14 8"
-                    transform="matrix(-1 0 0 1 16 0)"
-                />
-            </svg>
-        </div>
+    <div className="centerButton">
+        <svg
+            fill={disabled ? theme.disabled : '#404B5A'}
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            data-test={dataTest}
+        >
+            <polygon
+                points="8 2 6.94 3.06 11.13 7.25 2 7.25 2 8.75 11.13 8.75 6.94 12.94 8 14 14 8"
+                transform="matrix(-1 0 0 1 16 0)"
+            />
+        </svg>
         <style jsx>{centerButtonStyles}</style>
-    </>
+    </div>
 )
 
 IconRemoveIndividual.propTypes = {
