fe7893dd9d1d013c95b1336ad158af7c3a8c8be3
chisom chima <chimachisom360@gmail.com>
2024-02-19 12:43:02 +0100

    feat: update modal accessibility improvements
    feat: update modal accessibility improvements

--- components/modal/src/modal/modal.test.js
+++ components/modal/src/modal/modal.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import React from 'react'
+import { CloseButton } from './close-button.js'
 import { Modal } from './modal.js'
 
 describe('Modal', () => {
+    describe('Modal Accessibility', () => {
+        it('closes when ESC key is pressed', () => {
+            const onCloseMock = jest.fn()
+            render(<Modal onClose={onCloseMock} />)
+
+            const modalElement = screen.getByRole('dialog')
+            fireEvent.keyDown(modalElement, { key: 'Escape', code: 'Escape' })
+            expect(onCloseMock).toHaveBeenCalled()
+        })
+
+        it('does not close when "Enter" is pressed', () => {
+            const onCloseMock = jest.fn()
+            render(<Modal onClose={onCloseMock} />)
+
+            const modalElement = screen.getByRole('dialog')
+            fireEvent.keyDown(modalElement, { key: 'Enter', code: 'Enter' })
+            expect(onCloseMock).not.toHaveBeenCalled()
+        })
+
+        it('does not close when "SpaceBar" is pressed', () => {
+            const onCloseMock = jest.fn()
+            render(<Modal onClose={onCloseMock} />)
+
+            const modalElement = screen.getByRole('dialog')
+            fireEvent.keyDown(modalElement, { key: ' ', code: ' ' })
+            expect(onCloseMock).not.toHaveBeenCalled()
+        })
+
+        it('has a close button with proper accessibility attributes', async () => {
+            render(<CloseButton />)
+            const closeButton = await screen.findByLabelText(
+                'Close modal dialog'
+            )
+
+            expect(closeButton).toBeInTheDocument()
+            expect(closeButton.tagName).toBe('BUTTON')
+        })
+    })
+
     describe('Regular dimensions', () => {
         it('has the correct dimension styles in its default state', () => {
             render(<Modal />)
