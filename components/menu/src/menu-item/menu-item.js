35b9708edb566cf374324620d86cb52c28230ceb
Diana Nanyanzi <diana@dhis2.org>
2024-03-06 19:33:06 +0300

    feat: add keyboard navigation
    - Use Up and Down arrows to move through menu items list
    - Use Enter and Space keys to trigger a menu item in menu

--- components/menu/src/menu-item/menu-item.js
+++ components/menu/src/menu-item/menu-item.js
@@ -41,6 +41,7 @@ const MenuItem = ({
     toggleSubMenu,
     suffix,
     checkbox,
+    selected,
 }) => {
     const menuItemRef = useRef()
 
@@ -51,13 +52,13 @@ const MenuItem = ({
                     destructive,
                     disabled,
                     dense,
-                    active: active || showSubMenu,
+                    active: active || showSubMenu || selected,
                     'with-chevron': children || chevron,
                 })}
-                ref={menuItemRef}
                 data-test={dataTest}
                 role={checkbox ? 'menuitemcheckbox' : 'menuitem'}
                 aria-disabled={disabled ? disabled : false}
+                tabIndex={-1}
             >
                 <a
                     target={target}
@@ -123,6 +124,7 @@ MenuItem.propTypes = {
     icon: PropTypes.node,
     /** Text in the menu item */
     label: PropTypes.node,
+    selected: PropTypes.bool,
     /** When true, nested menu items are shown in a Popper */
     showSubMenu: PropTypes.bool,
     /** A supporting element shown at the end of the menu item */
--- components/menu/src/menu/menu.js
+++ components/menu/src/menu/menu.js
@@ -1,37 +1,138 @@
 import PropTypes from 'prop-types'
-import React, { Children, cloneElement, isValidElement } from 'react'
+import React, {
+    Children,
+    cloneElement,
+    isValidElement,
+    useEffect,
+    useState,
+    useRef,
+} from 'react'
 
-const Menu = ({ children, className, dataTest, dense }) => (
-    <ul className={className} data-test={dataTest} role="menu">
-        {Children.map(children, (child, index) =>
-            isValidElement(child)
-                ? cloneElement(child, {
-                      dense:
-                          typeof child.props.dense === 'boolean'
-                              ? child.props.dense
-                              : dense,
-                      hideDivider:
-                          typeof child.props.hideDivider !== 'boolean' &&
-                          index === 0
-                              ? true
-                              : child.props.hideDivider,
-                  })
-                : child
-        )}
+const Menu = ({ children, className, dataTest, dense }) => {
+    const [selectedIndex, setSelectedIndex] = useState(-1)
+    const menuRef = useRef(null)
 
-        <style jsx>{`
-            ul {
-                display: block;
-                position: relative;
-                width: 100%;
-                margin: 0;
+    useEffect(() => {
+        if (!menuRef.current) {
+            return
+        }
+        const menu = menuRef.current
+        const childrenArray = Children.toArray(children)
 
-                padding: 0;
-                user-select: none;
+        const handleIndex = (index, action) => {
+            let current_position = index
+            if (current_position === -1) {
+                return current_position
             }
-        `}</style>
-    </ul>
-)
+            const current_element = childrenArray[current_position]
+            if (
+                ['MenuSectionHeader', 'MenuDivider'].includes(
+                    current_element.type?.name
+                ) ||
+                current_element.props.disabled
+            ) {
+                current_position = action === 'up' ? index - 1 : index + 1
+            }
+
+            if (current_position > childrenArray.length - 1) {
+                current_position = 0
+            } else if (current_position < 0) {
+                current_position = childrenArray.length - 1
+            }
+
+            return current_position
+        }
+
+        const handleDirection = (event) => {
+            let active = selectedIndex
+            const { key } = event
+
+            if (key === 'ArrowUp') {
+                if (active > 0) {
+                    active = active - 1
+                    active = handleIndex(active, 'up')
+                    setSelectedIndex(active)
+                }
+            }
+            if (key === 'ArrowDown') {
+                if (active < childrenArray.length - 1) {
+                    active = active + 1
+                    active = handleIndex(active, 'down')
+                    setSelectedIndex(active)
+                }
+            }
+        }
+
+        const handleActionKeys = (event) => {
+            if (selectedIndex > -1) {
+                const { target } = event
+                const child = target.children[selectedIndex]
+                if (child.onclick) {
+                    child.click()
+                } else if (child.childNodes[0].onclick) {
+                    child.childNodes[0].click()
+                }
+            }
+        }
+
+        const handleKeyDown = (event) => {
+            event.preventDefault()
+
+            if (event.key === ' ' || event.key === 'Enter') {
+                handleActionKeys(event)
+            }
+            if (event.key.startsWith('Arrow')) {
+                handleDirection(event)
+            }
+        }
+
+        menu.addEventListener('keydown', handleKeyDown)
+
+        return () => {
+            menu.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [children, selectedIndex])
+
+    return (
+        <ul
+            className={className}
+            data-test={dataTest}
+            role="menu"
+            tabIndex={0}
+            ref={menuRef}
+        >
+            {Children.map(children, (child, index) => {
+                return isValidElement(child)
+                    ? cloneElement(child, {
+                          dense:
+                              typeof child.props.dense === 'boolean'
+                                  ? child.props.dense
+                                  : dense,
+                          hideDivider:
+                              typeof child.props.hideDivider !== 'boolean' &&
+                              index === 0
+                                  ? true
+                                  : child.props.hideDivider,
+                          selected:
+                              index === selectedIndex && !child.props.disabled,
+                      })
+                    : child
+            })}
+
+            <style jsx>{`
+                ul {
+                    display: block;
+                    position: relative;
+                    width: 100%;
+                    margin: 0;
+
+                    padding: 0;
+                    user-select: none;
+                }
+            `}</style>
+        </ul>
+    )
+}
 
 Menu.defaultProps = {
     dataTest: 'dhis2-uicore-menulist',
