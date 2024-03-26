e40573b1bfa50f83820fd865119298cf85af367e
Alaa Yahia <alaay873@live.com>
2024-03-26 10:51:01 +0200

    feat(TabBar): accessibility improvements for TabBar & Tab

--- components/tab/src/tab-bar/tabs.js
+++ components/tab/src/tab-bar/tabs.js
@@ -1,26 +1,66 @@
 import { colors } from '@dhis2/ui-constants'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Tabs = ({ children, fixed, dataTest }) => (
-    <div className={cx({ fixed })} data-test={dataTest}>
-        {children}
+const Tabs = ({ children, fixed, dataTest }) => {
+    const tabContainer = useRef(null)
 
-        <style jsx>{`
-            div {
-                display: flex;
-                align-items: flex-start;
-                position: relative;
-                overflow: hidden;
-                box-shadow: inset 0 -1px 0 0 ${colors.grey400};
-                flex-wrap: nowrap;
-                flex-grow: 1;
-                background: ${colors.white};
-            }
-        `}</style>
-    </div>
-)
+    const handleKeyDown = (event) => {
+        const currentFocus = document.activeElement
+        if (
+            !tabContainer.current ||
+            !tabContainer.current.contains(currentFocus)
+        ) {
+            return
+        }
+
+        const role = currentFocus.getAttribute('role')
+        if (role !== 'tab') {
+            return
+        }
+        const tabs = Array.from(
+            tabContainer.current.querySelectorAll('[role="tab"]')
+        )
+        const currentIndex = tabs.indexOf(currentFocus)
+
+        if (event.key === 'ArrowRight') {
+            event.preventDefault()
+            const nextIndex = (currentIndex + 1) % tabs.length
+            tabs[nextIndex].focus()
+        }
+        if (event.key === 'ArrowLeft') {
+            event.preventDefault()
+            const prevIndex = (currentIndex - 1 + tabs.length) % tabs.length
+            tabs[prevIndex].focus()
+        }
+    }
+
+    return (
+        <div
+            className={cx({ fixed })}
+            ref={tabContainer}
+            data-test={dataTest}
+            role="tablist"
+            onKeyDown={handleKeyDown}
+        >
+            {children}
+
+            <style jsx>{`
+                div {
+                    display: flex;
+                    align-items: flex-start;
+                    position: relative;
+                    overflow: hidden;
+                    box-shadow: inset 0 -1px 0 0 ${colors.grey400};
+                    flex-wrap: nowrap;
+                    flex-grow: 1;
+                    background: ${colors.white};
+                }
+            `}</style>
+        </div>
+    )
+}
 
 Tabs.propTypes = {
     dataTest: PropTypes.string.isRequired,
