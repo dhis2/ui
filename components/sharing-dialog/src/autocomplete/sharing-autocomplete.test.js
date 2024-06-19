b5f058ab47984914fc88c977d1860dfacffbb379
Diana Nanyanzi <diana@dhis2.org>
2024-06-19 07:58:05 +0300

    fix: update role to menuitem in sharing-dialog-autocomplete test

--- components/sharing-dialog/src/autocomplete/sharing-autocomplete.test.js
+++ components/sharing-dialog/src/autocomplete/sharing-autocomplete.test.js
@@ -1,5 +1,10 @@
 import { CustomDataProvider } from '@dhis2/app-runtime'
-import { render, screen, waitFor } from '@testing-library/react'
+import {
+    render,
+    screen,
+    waitFor,
+    waitForElementToBeRemoved,
+} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import React, { useState } from 'react'
@@ -43,17 +48,18 @@ describe('SharingAutocomplete', () => {
         userEvent.type(screen.getByRole('textbox'), searchString)
         expect(screen.getByRole('textbox')).toHaveValue(searchString)
 
-        await waitFor(() => screen.getByRole('listitem'))
-        userEvent.click(screen.getByRole('listitem').querySelector('a'))
+        await waitForElementToBeRemoved(() => screen.getByRole('progressbar'))
+
+        userEvent.click(screen.getByRole('menuitem'))
         expect(screen.getByRole('textbox')).toHaveValue(userDisplayName)
 
         try {
-            await waitFor(() => screen.getByRole('listitem'), { timeout: 1 })
+            await waitFor(() => screen.getByRole('menuitem'), { timeout: 1 })
         } catch (error) {
-            if (!error.message.startsWith('Unable to find role="listitem"')) {
+            if (!error.message.startsWith('Unable to find role="menuitem"')) {
                 throw error
             }
         }
-        expect(screen.queryByRole('listitem')).not.toBeInTheDocument()
+        expect(screen.queryByRole('menuitem')).not.toBeInTheDocument()
     })
 })
