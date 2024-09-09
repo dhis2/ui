a24746044521746918fc985b8dc353911e892421
Rene Pot <rene@dhis2.org>
2024-09-09 09:27:45 +0100

    docs: fix demo url

--- docs/src/constants.js
+++ docs/src/constants.js
@@ -1,2 +1,2 @@
-// export const DEMO_URL = 'https://ui.dhis2.nu/demo'
-export const DEMO_URL = 'http://127.0.0.1:8080'
+// export const DEMO_URL = 'http://127.0.0.1:8080'
+export const DEMO_URL = '/demo'
