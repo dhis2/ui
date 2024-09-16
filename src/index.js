1976bac1cc4b1518e2f83042eec52045571c1660
Mohammer5 <jan-gerke@dhis2.org>
2024-09-16 11:21:04 +0200

    chore: fix src/index.js issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * from './components/index.js'
 export * from './constants/index.js'
+export * from './forms/index.js'
 export * from './icons/index.js'
--- src/index.test.js
+++
@@ -1,5 +0,0 @@
-import * as allExports from './index.js'
-
-test('exports', () => {
-    expect(Object.keys(allExports)).toEqual([])
-})
