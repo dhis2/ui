d1cbf0a351ece26121423744148c490ffcb38fba
Mohammer5 <jan-gerke@dhis2.org>
2024-09-17 15:53:38 +0200

    chore: remove types

--- types/portal.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export interface PortalProps {
-    children?: React.ReactNode
-    disable?: boolean
-    node?: React.ReactNode
-}
-
-export const Portal: React.FC<PortalProps>
--- types/required.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export interface RequiredProps {
-    dataTest: string
-}
-
-export const Required: React.FC<RequiredProps>
