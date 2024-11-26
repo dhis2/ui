b57b81c190fe874a3318e45aac091b7a1418bd76
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2024-11-26 12:32:56 +0100

    fix: prevent prop-types error by making tabIndex an optional prop (#1640)

--- components/select/src/select/input-wrapper.js
+++ components/select/src/select/input-wrapper.js
@@ -100,13 +100,13 @@ const InputWrapper = ({
 InputWrapper.propTypes = {
     dataTest: PropTypes.string.isRequired,
     inputRef: PropTypes.object.isRequired,
-    tabIndex: PropTypes.string.isRequired,
     onToggle: PropTypes.func.isRequired,
     children: PropTypes.element,
     className: PropTypes.string,
     dense: PropTypes.bool,
     disabled: PropTypes.bool,
     error: sharedPropTypes.statusPropType,
+    tabIndex: PropTypes.string,
     valid: sharedPropTypes.statusPropType,
     warning: sharedPropTypes.statusPropType,
 }
